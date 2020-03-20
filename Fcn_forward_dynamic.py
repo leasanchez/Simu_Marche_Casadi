0a6f9e60cd3976e4b8000874f1323c3d997fe8d0
Lea Sanchez <lea.sanchez@umontreal.ca>
2020-03-20 15:20:45 -0400

    Add parameter for isometric forces

--- Fcn_forward_dynamic.py
+++ Fcn_forward_dynamic.py
@@ -1,16 +1,27 @@
 import biorbd
 from casadi import *
 
-def ffcn_no_contact(x, u):
+def ffcn_no_contact(x, u, p):
     # SET MODEL
     model = biorbd.Model('/home/leasanchez/programmation/Simu_Marche_Casadi/ModelesS2M/ANsWER_Rleg_6dof_17muscle_0contact.bioMod')
+
+    # EXTRACT INITIAL MAXIMAL ISOMETRIC FORCES FROM THE MODEL
+    FISO0 = np.zeros(model.nbMuscleTotal())
+    n_muscle = 0
+    for nGrp in range(model.nbMuscleGroups()):
+        for nMus in range(model.muscleGroup(nGrp).nbMuscles()):
+            FISO0[n_muscle] = model.muscleGroup(nGrp).muscle(nMus).characteristics().forceIsoMax()
+            n_muscle += 1
+
+    # FIND THE PARAMETERS P OPTIMISING THE MAXIMUM ISOMETRIC FORCES -- MODEL SWING
+    Set_forceISO_max_swing = external('libforce_iso_max', 'libforce_iso_max.so', {'enable_fd': True})
+    forceISO = p[0] * p[1:] * FISO0
+    Set_forceISO_max_swing(forceISO)
 
     # INPUT
     Q              = x[:model.nbQ()]                            # states
     dQ             = x[model.nbQ():2 * model.nbQ()]
     activations    = u[: model.nbMuscleTotal()]                 # controls
-
-
 
     # COMPUTE MOTOR JOINT TORQUES
     muscularJointTorque = external('libmuscular_joint_torque', 'libmuscular_joint_torque.so', {'enable_fd': True})
@@ -30,9 +41,22 @@ def ffcn_no_contact(x, u):
 
 
 
-def ffcn_contact(x, u):
+def ffcn_contact(x, u, p):
     # SET MODEL
     model = biorbd.Model('/home/leasanchez/programmation/Simu_Marche_Casadi/ModelesS2M/ANsWER_Rleg_6dof_17muscle_1contact.bioMod')
+
+    # EXTRACT INITIAL MAXIMAL ISOMETRIC FORCES FROM THE MODEL
+    FISO0 = np.zeros(model.nbMuscleTotal())
+    n_muscle = 0
+    for nGrp in range(model.nbMuscleGroups()):
+        for nMus in range(model.muscleGroup(nGrp).nbMuscles()):
+            FISO0[n_muscle] = model.muscleGroup(nGrp).muscle(nMus).characteristics().forceIsoMax()
+            n_muscle += 1
+
+    # FIND THE PARAMETERS P OPTIMISING THE MAXIMUM ISOMETRIC FORCES -- MODEL STANCE
+    Set_forceISO_max = external('libforce_iso_max_stance', 'libforce_iso_max_stance.so', {'enable_fd': True})
+    forceISO         = p[0] * p[1:] * FISO0
+    Set_forceISO_max(forceISO)
 
     # INPUT
     Q           = x[:model.nbQ()]                            # states
