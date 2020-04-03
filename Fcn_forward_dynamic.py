3a325e54b2b1a08f781ac5ff4c01cf492e391e20
Lea Sanchez <lea.sanchez@umontreal.ca>
2020-04-03 17:10:13 -0400

    Added parameters for muscle in dynamics

--- Fcn_forward_dynamic.py
+++ Fcn_forward_dynamic.py
@@ -1,78 +1,67 @@
 import biorbd
 from casadi import *
 
-def Get_initial_forceiso(model):
-    FISO0 = np.zeros(model.nbMuscleTotal())
-    n_muscle = 0
-    for nGrp in range(model.nbMuscleGroups()):
-        for nMus in range(model.muscleGroup(nGrp).nbMuscles()):
-            FISO0[n_muscle] = model.muscleGroup(nGrp).muscle(nMus).characteristics().forceIsoMax()
-            n_muscle += 1
-    return FISO0
+class Dynamics:
+    @staticmethod
+    def Set_parameter_forceiso(model, p):
+        n_muscle = 0
+        for nGrp in range(model.nbMuscleGroups()):
+            for nMus in range(model.muscleGroup(nGrp).nbMuscles()):
+                fiso_init = model.muscleGroup(nGrp).muscle(nMus).characteristics().forceIsoMax().to_mx()
+                model.muscleGroup(nGrp).muscle(nMus).characteristics().setForceIsoMax(p[n_muscle] * fiso_init)
+                n_muscle += 1
+        return [0]
 
-def Set_parameter_forceiso(model, FISO0, p):
-    n_muscle = 0
-    for nGrp in range(model.nbMuscleGroups()):
-        for nMus in range(model.muscleGroup(nGrp).nbMuscles()):
-            model.muscleGroup(nGrp).muscle(nMus).characteristics().setforceisomax(p[n_muscle] * FISO0[n_muscle])
-            n_muscle += 1
-    return [0]
+    @staticmethod
+    def articular_torque(model, activations, Q, dQ):
+        states = biorbd.VecBiorbdMuscleStateDynamics(model.nbMuscleTotal())
+        for n_muscle in range(model.nbMuscleTotal()):
+            states[n_muscle].setActivation(activations[n_muscle])
+        joint_torque = model.muscularJointTorque(states, Q, dQ).to_mx()
+        return joint_torque
 
-def articular_torque(model, activations, Q, dQ):
-    states = biorbd.VecBiorbdMuscleStateDynamics(model.nbMuscleTotal())
-    for n_muscle in range(model.nbMuscleTotal()):
-        states[n_muscle].setActivation(activations[n_muscle])
-    joint_torque = model.muscularJointTorque(states, Q, dQ).to_mx()
-    return joint_torque
+    @staticmethod
+    def ffcn_no_contact(x, u, p):
+        # SET MODEL
+        model = biorbd.Model('/home/leasanchez/programmation/Simu_Marche_Casadi/ModelesS2M/ANsWER_Rleg_6dof_17muscle_0contact.bioMod')
 
-def ffcn_no_contact(x, u, p):
-    # SET MODEL
-    model = biorbd.Model('/home/leasanchez/programmation/Simu_Marche_Casadi/ModelesS2M/ANsWER_Rleg_6dof_17muscle_0contact.bioMod')
+        # FIND THE PARAMETERS P OPTIMISING THE MAXIMUM ISOMETRIC FORCES -- MODEL SWING
+        Dynamics.Set_parameter_forceiso(model, p)
 
-    # EXTRACT INITIAL MAXIMAL ISOMETRIC FORCES FROM THE MODEL
-    # FISO0 = Get_initial_forceiso(model)
+        # INPUT
+        Q              = x[:model.nbQ()]                            # states
+        dQ             = x[model.nbQ():2 * model.nbQ()]
+        activations    = u[: model.nbMuscleTotal()]                 # controls
+        torque         = u[model.nbMuscleTotal():]
 
-    # FIND THE PARAMETERS P OPTIMISING THE MAXIMUM ISOMETRIC FORCES -- MODEL SWING
-    # Set_parameter_forceiso(model, FISO0, p)
+        # COMPUTE MOTOR JOINT TORQUES
+        joint_torque = Dynamics.articular_torque(model, activations, Q, dQ)
+        joint_torque += torque                  # add residual torques
 
-    # INPUT
-    Q              = x[:model.nbQ()]                            # states
-    dQ             = x[model.nbQ():2 * model.nbQ()]
-    activations    = u[: model.nbMuscleTotal()]                 # controls
-    torque         = u[model.nbMuscleTotal():]
+        # COMPUTE THE ACCELERATION -- FORWARD DYNAMICS
+        ddQ = model.ForwardDynamics(Q, dQ, joint_torque).to_mx()
 
-    # COMPUTE MOTOR JOINT TORQUES
-    joint_torque = articular_torque(model, activations, Q, dQ)
-    joint_torque += torque                  # add residual torques
+        return vertcat(dQ, ddQ)
 
-    # COMPUTE THE ANGULAR ACCELERATION BY FORWARD DYNAMICS
-    ddQ = model.ForwardDynamics(Q, dQ, joint_torque).to_mx()
+    @staticmethod
+    def ffcn_contact(x, u, p):
+        # SET MODEL
+        model = biorbd.Model('/home/leasanchez/programmation/Simu_Marche_Casadi/ModelesS2M/ANsWER_Rleg_6dof_17muscle_1contact.bioMod')
 
-    return vertcat(dQ, ddQ)
+        # FIND THE PARAMETERS P OPTIMISING THE MAXIMUM ISOMETRIC FORCES -- MODEL STANCE
+        Dynamics.Set_parameter_forceiso(model, p)
 
+        # INPUT
+        Q           = x[:model.nbQ()]                            # states
+        dQ          = x[model.nbQ():2*model.nbQ()]
+        activations = u[: model.nbMuscleTotal()]                 # controls
+        torque      = u[model.nbMuscleTotal():]
 
+        # COMPUTE MOTOR JOINT TORQUES
+        joint_torque  = Dynamics.articular_torque(model, activations, Q, dQ)
+        joint_torque += torque                  # add residual torques
 
-def ffcn_contact(x, u, p):
-    # SET MODEL
-    model = biorbd.Model('/home/leasanchez/programmation/Simu_Marche_Casadi/ModelesS2M/ANsWER_Rleg_6dof_17muscle_1contact.bioMod')
+        # COMPUTE THE ACCELERATION -- FORWARD DYNAMICS
+        ddQ = model.ForwardDynamicsConstraintsDirect(Q, dQ, joint_torque).to_mx()
 
-    # EXTRACT INITIAL MAXIMAL ISOMETRIC FORCES FROM THE MODEL
-    # FISO0 = Get_initial_forceiso(model)
-
-    # FIND THE PARAMETERS P OPTIMISING THE MAXIMUM ISOMETRIC FORCES -- MODEL STANCE
-    # Set_parameter_forceiso(model, p)
-
-    # INPUT
-    Q           = x[:model.nbQ()]                            # states
-    dQ          = x[model.nbQ():2*model.nbQ()]
-    activations = u[: model.nbMuscleTotal()]                 # controls
-    torque      = u[model.nbMuscleTotal():]
-
-    # COMPUTE MOTOR JOINT TORQUES
-    joint_torque = articular_torque(model, activations, Q, dQ)
-    joint_torque += torque                  # add residual torques
-
-    # COMPUTE THE ANGULAR ACCELERATION BY FORWARD DYNAMICS
-    ddQ = model.ForwardDynamicsConstraintsDirect(Q, dQ, joint_torque).to_mx()
-
-    return vertcat(dQ, ddQ)
+        return vertcat(dQ, ddQ)
--- Marche_Fcn_Integration.py
+++ Marche_Fcn_Integration.py
@@ -1,5 +1,3 @@
-from Fcn_forward_dynamic import ffcn_contact, ffcn_no_contact
-
 
 # Integration Euler
 def int_Euler_swing(T, nbNoeuds, neuler, x, u):
@@ -48,7 +46,6 @@ def int_RK4_stance(T, nbNoeuds, nkutta, x, u):
 # Integration Runge Kutta 4
 def int_RK4(fun, params, x, u, p):
     nkutta = params.nkutta
-
     if fun.name() == 'ffcn_contact':
         T        = params.T_stance
         nbNoeuds = params.nbNoeuds_stance
@@ -60,13 +57,13 @@ def int_RK4(fun, params, x, u, p):
     dt = dn / nkutta                 # Time step for iteration
     xj = x
     for i in range(nkutta):
-        k1 = fun(xj, u)
+        k1 = fun(xj, u, p)
         x2 = xj + (dt/2)*k1
-        k2 = fun(x2, u)
+        k2 = fun(x2, u, p)
         x3 = xj + (dt/2)*k2
-        k3 = fun(x3, u)
+        k3 = fun(x3, u, p)
         x4 = xj + dt*k3
-        k4 = fun(x4, u)
+        k4 = fun(x4, u, p)
 
         xj += dt/6 * (k1 + 2*k2 + 2*k3 + k4)
     return xj
