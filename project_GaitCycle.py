f3a5b6084e2e13299d5e00d26646042e78564872
Lea Sanchez <lea.sanchez@umontreal.ca>
2020-04-07 10:07:19 -0400

    Added parameters and casadi fcn

--- project_GaitCycle.py
+++ project_GaitCycle.py
@@ -3,14 +3,14 @@ import numpy as np
 
 # add classes
 from Define_parameters import Parameters
+from Fcn_Objective import Fcn_Objective
+from Fcn_forward_dynamic import Dynamics
 
 # add fcn
 import LoadData
-from Fcn_InitialGuess import load_initialguess_muscularExcitation, load_initialguess_q
+import Fcn_print_data
+from Fcn_InitialGuess import load_initialguess_q, load_initialguess_muscularExcitation
 from Marche_Fcn_Integration import int_RK4
-import Fcn_forward_dynamic # ffcn_contact, ffcn_no_contact
-import Fcn_Objective # fcn_objective_activation, fcn_objective_emg, fcn_objective_markers, fcn_objective_GRF
-import Fcn_print_data # save_GRF_real, save_Markers_real, save_EMG_real, save_params, save_bounds, save_initialguess
 
 # SET PARAMETERS
 params = Parameters()
@@ -21,9 +21,9 @@ u  = MX.sym("u", params.nbU)
 activation  = u[:params.nbMus]                                # muscular activation
 torque      = u[params.nbMus:]                                # residual joint torque
 
-# # PARAMETERS
-# p  = MX.sym("p", params.nP)                                   # maximal isometric force adjustment
-p = [1, 0.2,0.21, 0.524, 0.223, 0.2, 0.2, 1.68, 0.28, 0.2, 2.84, 0.2, 0.2, 0.38, 4.97, 5, 1.18, 5] # MUSCOD results
+# PARAMETERS
+p  = MX.sym("p", params.nP)                                   # maximal isometric force adjustment
+
 # STATE
 x  = MX.sym("x", params.nbX)
 q  = x[:params.nbQ]                                           # generalized coordinates
@@ -31,15 +31,21 @@ dq = x[params.nbQ: 2 * params.nbQ]                            # velocities
 
 # ----------------------------- Define casadi function -----------------------------------------------------------------
 ffcn_contact = casadi.Function("ffcn_contact",
-                                [x, u],
-                                [Fcn_forward_dynamic.ffcn_contact(x, u, p)],
-                                ["states", "controls"],
+                                [x, u, p],
+                                [Dynamics.ffcn_contact(x, u, p)],
+                                ["states", "controls", "parameters"],
                                 ["statesdot"]).expand()
 
+compute_GRF = casadi.Function("compute_GRF",
+                              [x, u, p],
+                              [Dynamics.compute_GRF(x, u, p)],
+                              ["states", "controls", "parameters"],
+                              ["GRF"]).expand()
+
 ffcn_no_contact = casadi.Function("ffcn_no_contact",
-                                   [x, u],
-                                   [Fcn_forward_dynamic.ffcn_no_contact(x, u, p)],
-                                   ["states", "controls"],
+                                   [x, u, p],
+                                   [Dynamics.ffcn_no_contact(x, u, p)],
+                                   ["states", "controls", "parameters"],
                                    ["statesdot"]).expand()
 
 
@@ -51,22 +57,15 @@ ffcn_no_contact = casadi.Function("ffcn_no_contact",
 M_real_stance = LoadData.load_data_markers(params, 'stance')
 M_real_swing  = LoadData.load_data_markers(params, 'swing')
 
-M_real          = np.zeros((3, params.nbMarker, (params.nbNoeuds + 1)))
-M_real[0, :, :] = np.hstack([M_real_stance[0, :, :], M_real_swing[0, :, :]])
-M_real[1, :, :] = np.hstack([M_real_stance[1, :, :], M_real_swing[1, :, :]])
-M_real[2, :, :] = np.hstack([M_real_stance[2, :, :], M_real_swing[2, :, :]])
-
 # MUSCULAR EXCITATION
 U_real_swing  = LoadData.load_data_emg(params, 'swing')
 U_real_stance = LoadData.load_data_emg(params, 'stance')
-U_real        = np.hstack([U_real_stance, U_real_swing])
 
 
 # ----------------------------- Movement -------------------------------------------------------------------------------
 U = MX.sym("U", params.nbU * params.nbNoeuds)          # controls
 X = MX.sym("X", params.nbX * (params.nbNoeuds + 1))    # states
-P = p
-# P = MX.sym("P", params.nP)                           # parameters
+P = MX.sym("P", params.nP)                             # parameters
 G = []                                                 # equality constraints
 Ja = 0                                                 # objective function for muscle activation
 Jm = 0                                                 # objective function for markers
@@ -82,8 +81,8 @@ for k in range(params.nbNoeuds_stance):
     G.append(X[params.nbX * (k + 1): params.nbX * (k + 2)] - int_RK4(ffcn_contact, params, Xk, Uk, P))
 
     # OBJECTIVE FUNCTION
-    [grf, Jr] = Fcn_Objective.fcn_objective_GRF(params.wR, Xk, Uk, GRF_real[:, k])                                                    # tracking ground reaction --> stance
-    JR += Jr
+    GRF = compute_GRF(Xk, Uk, P)
+    JR += Fcn_Objective.fcn_objective_GRF_casadi(params.wR, GRF, GRF_real[:, k])                                                      # tracking ground reaction --> stance
     Jm += Fcn_Objective.fcn_objective_markers(params.wMa, params.wMt, Xk[: params.nbQ], M_real_stance[:, :, k], 'stance')             # tracking marker
     Je += Fcn_Objective.fcn_objective_emg(params.wU, Uk, U_real_stance[:, k])                                                         # tracking emg
     Ja += Fcn_Objective.fcn_objective_activation(params.wL, Uk)                                                                       # min muscle activations (no EMG)
@@ -108,71 +107,72 @@ lbg = [0] * params.nbX * params.nbNoeuds
 ubg = [0] * params.nbX * params.nbNoeuds
 
 # ----------------------------- Bounds on w ----------------------------------------------------------------------------
-# activation - excitation musculaire
-min_A = 1e-3                        # 0 exclusif
+# controls
+min_A = 1e-3
 max_A = 1
-lowerbound_u = [min_A] * params.nbMus + [-1000] + [-2000] + [-200]
-upperbound_u = [max_A] * params.nbMus + [1000]  + [2000]  + [200]
+lowerbound_u = [min_A] * params.nbMus + [-2000]*params.nbQ
+upperbound_u = [max_A] * params.nbMus + [2000]*params.nbQ
 lbu = (lowerbound_u) * params.nbNoeuds
 ubu = (upperbound_u) * params.nbNoeuds
 
-# q et dq
+# states
 lowerbound_x = [-10, -0.5, -np.pi/4, -np.pi/9, -np.pi/2, -np.pi/3, 0.5, -0.5, -1.7453, -5.2360, -5.2360, -5.2360]
 upperbound_x = [10, 1.5, np.pi/4, np.pi/3, 0.0873, np.pi/9, 1.5, 0.5, 1.7453, 5.2360, 5.2360, 5.2360]
 lbX   = (lowerbound_x) * (params.nbNoeuds + 1)
 ubX   = (upperbound_x) * (params.nbNoeuds + 1)
 
 # parameters
-min_pg = 1
 min_p  = 0.2
 max_p  = 5
-max_pg = 1
-lbp = [min_pg] + [min_p] * params.nbMus
-ubp = [max_pg] + [max_p] * params.nbMus
+lbp = [min_p] * params.nbMus
+ubp = [max_p] * params.nbMus
 
 
-lbx = vertcat(lbu, lbX)
-ubx = vertcat(ubu, ubX)
+lbx = vertcat(lbu, lbX, lbp)
+ubx = vertcat(ubu, ubX, ubp)
 
 # ----------------------------- Initial guess --------------------------------------------------------------------------
 init_A = 0.1
 
 # CONTROL
 u0                    = np.zeros((params.nbU, params.nbNoeuds))
-u0[: params.nbMus, :] = load_initialguess_muscularExcitation(np.hstack([U_real_stance, U_real_swing]))
-# u0[: nbMus, :]   = np.zeros((nbMus, nbNoeuds)) + init_A
-u0[params.nbMus + 0, :] = [0] * params.nbNoeuds                                  # pelvis forces
+# u0[: params.nbMus, :] = load_initialguess_muscularExcitation(np.hstack([U_real_stance[:, :-1], U_real_swing[:, :-1]]))
+u0[: params.nbMus, :]   = np.zeros((params.nbMus, params.nbNoeuds)) + init_A
 u0[params.nbMus + 1, :] = [-500] * params.nbNoeuds_stance + [0] * params.nbNoeuds_swing
-u0[params.nbMus + 2, :] = [0] * params.nbNoeuds
 
 # STATE
-q0_stance = load_initialguess_q(params, 'stance')
-q0_swing  = load_initialguess_q(params, 'swing')
-q0        = np.hstack([q0_stance, q0_swing])
-dq0       = np.gradient(q0)
-dq0       = dq0[0]
+q0  = load_initialguess_q(params, 'cycle')
+dq0 = np.gradient(q0)
+dq0 = dq0[0]
 
 X0                 = np.zeros((params.nbX, (params.nbNoeuds + 1)))
 X0[:params.nbQ, :] = q0
 X0[params.nbQ: 2 * params.nbQ, :] = dq0
 
 # PARAMETERS
-p0 = [1] + [1] * params.nbMus
+p0 = [1] * params.nbMus
 
-w0 = vertcat(vertcat(*u0.T), vertcat(*X0.T))
+w0 = vertcat(vertcat(*u0.T), vertcat(*X0.T), p0)
 
-# ----------------------------- Save txt -------------------------------------------------------------------------------
-Fcn_print_data.save_GRF_real(params, GRF_real)
-Fcn_print_data.save_Markers_real(params, M_real)
-Fcn_print_data.save_EMG_real(params, U_real)
-Fcn_print_data.save_params(params)
-Fcn_print_data.save_bounds(params, lbx, ubx)
-Fcn_print_data.save_initialguess(params, u0, X0, p0)
+# # ----------------------------- Save txt -------------------------------------------------------------------------------
+# M_real          = np.zeros((3, params.nbMarker, (params.nbNoeuds + 1)))
+# M_real[0, :, :] = np.hstack([M_real_stance[0, :, :-1], M_real_swing[0, :, :]])
+# M_real[1, :, :] = np.hstack([M_real_stance[1, :, :-1], M_real_swing[1, :, :]])
+# M_real[2, :, :] = np.hstack([M_real_stance[2, :, :-1], M_real_swing[2, :, :]])
+#
+# U_real = np.hstack([U_real_stance[:, :-1], U_real_swing[:, :-1]])
+#
+# Fcn_print_data.save_GRF_real(params, GRF_real)
+# Fcn_print_data.save_Markers_real(params, M_real)
+# Fcn_print_data.save_EMG_real(params, U_real)
+# Fcn_print_data.save_params(params)
+# Fcn_print_data.save_bounds(params, lbx, ubx)
+# Fcn_print_data.save_initialguess(params, u0, X0, p0)
 
 
 # ----------------------------- Solver ---------------------------------------------------------------------------------
-w = vertcat(U, X)
-J = Ja + Je + Jm + JR
+w = vertcat(U, X, P)
+J = Ja + Je + Jm + JR + Jt
 
 nlp    = {'x': w, 'f': J, 'g': vertcat(*G)}
 opts   = {"ipopt.tol": 1e-2, "ipopt.linear_solver": "ma57"}
