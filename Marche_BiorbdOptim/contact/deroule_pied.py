dccd4dd7f9443e230a8743f5422b1d464ba1ea07
Lea Sanchez <lea.sanchez@umontreal.ca>
2020-06-15 16:34:15 -0400

    [WIP] 3 phases

--- Marche_BiorbdOptim/contact/deroule_pied.py
+++ Marche_BiorbdOptim/contact/deroule_pied.py
@@ -20,6 +20,80 @@ from biorbd_optim import (
     StateTransition,
 )
 
+def get_dispatch_forefoot_contact_forces(grf_ref, M_ref, coord, nb_shooting):
+    p = 0.5  # repartition entre les 2 points
+    px = np.repeat(0.5, number_shooting_points[2] + 1)
+    px[:5] = np.linspace(0, 0.5, 5)
+
+    F_Meta1 = MX.sym("F_Meta1", 3 * (nb_shooting + 1), 1)
+    F_Meta5 = MX.sym("F_Meta1", 3 * (nb_shooting + 1), 1)
+    M_Meta1 = MX.sym("M_Meta1", 3 * (nb_shooting + 1), 1)
+    M_Meta5 = MX.sym("M_Meta1", 3 * (nb_shooting + 1), 1)
+
+    objective = 0
+    lbg = []
+    ubg = []
+    constraint = []
+    for i in range(nb_shooting + 1):
+        # Aliases
+        fm1 = F_Meta1[3 * i: 3 * (i + 1)]
+        fm5 = F_Meta5[3 * i: 3 * (i + 1)]
+        mm1 = M_Meta1[3 * i: 3 * (i + 1)]
+        mm5 = M_Meta5[3 * i: 3 * (i + 1)]
+
+        # sum forces = 0 --> Fp1 + Fp2 = Ftrack
+        sf = fm1 + fm5
+        jf = sf - grf_ref[:, i]
+        objective += 100 * mtimes(jf.T, jf)
+
+        # sum moments = 0 --> Mp1_P1 + CP1xFp1 + Mp2_P2 + CP2xFp2 = Mtrack
+        sm = mm1 + dot(coord[0], fm1) + mm5 + dot(coord[1], fm5)
+        jm = sm - M_ref[:, i]
+        objective += 100 * mtimes(jm.T, jm)
+
+        # use of p to dispatch forces --> p*Fp1 - (1-p)*Fp2 = 0
+        jf2 = p * fm1[2] - (1 - p) * fm5[2]
+        jf3 = px[i] * p * fm1[0] - (1 - px[i]) * (1 - p) * fm5[0]
+        jf4 = p * fm1[1] - (1 - p) * fm5[1]
+        objective += dot(jf2, jf2) + dot(jf3, jf3) + dot(jf4, jf4)
+
+        # use of p to dispatch moments
+        jm2 = p * (mm1 + dot(coord[0], fm1)) - (1 - p) * (mm5 + dot(coord[1], fm5))
+        objective += mtimes(jm2.T, jm2)
+
+        # positive vertical force
+        constraint += (fm1[2], fm5[2])
+        lbg += [0] * 2
+        ubg += [1000] * 2
+
+        # non slipping --> -0.4*Fz < Fx < 0.4*Fz
+        constraint += ((-0.4 * fm1[2] - fm1[0]), (-0.4 * fm5[2] - fm5[0]))
+        lbg += [-1000] * 2
+        ubg += [0] * 2
+
+        constraint += ((0.4 * fm1[2] - fm1[0]), (0.4 * fm5[2] - fm5[0]))
+        lbg += [0] * 2
+        ubg += [1000] * 2
+
+    w = [F_Meta1, F_Meta5, M_Meta1, M_Meta5]
+    nlp = {'x': vertcat(*w), 'f': objective, 'g': vertcat(*constraint)}
+    opts = {"ipopt.tol": 1e-8, "ipopt.hessian_approximation": "exact"}
+    solver = nlpsol("solver", "ipopt", nlp, opts)
+    res = solver(x0=np.zeros(6 * 2 * (number_shooting_points[2] + 1)),
+                 lbx=-1000,
+                 ubx=1000,
+                 lbg=lbg,
+                 ubg=ubg)
+
+    FM1 = res['x'][:3 * (nb_shooting + 1)]
+    FM5 = res['x'][3 * (nb_shooting + 1): 6 * (nb_shooting + 1)]
+
+    grf_dispatch_ref = np.zeros((3 * 2, nb_shooting + 1))
+    for i in range(3):
+        grf_dispatch_ref[i, :] = np.array(FM1[i::3]).squeeze()
+        grf_dispatch_ref[i + 3, :] = np.array(FM5[i::3]).squeeze()
+    return grf_dispatch_ref
+
 def get_dispatch_contact_forces(grf_ref, M_ref, coord, nb_shooting):
     p_heel = np.linspace(0, 1, nb_shooting + 1)
     # p_heel = 1 - p_heel
@@ -58,8 +132,11 @@ def get_dispatch_contact_forces(grf_ref, M_ref, coord, nb_shooting):
 
         # --- Dispatch on different contact points ---
         # use of p to dispatch forces --> p_heel*Fh - (1-p_heel)*Fm = 0
-        jf2 = p_heel[i] * fh - ((1 - p_heel[i]) * (p * fm1 + (1 - p) * fm5))
-        objective += mtimes(jf2.T, jf2)
+        jf2 = p_heel[i] * fh[2] - ((1 - p_heel[i]) * (p * fm1[2] + (1 - p) * fm5[2]))
+        jf3 = p_heel[i] * fh[0] - ((1 - p_heel[i]) * (1 * fm1[0] + 0 * fm5[0]))
+        jf32 = fm5[0]
+        jf4 = p_heel[i] * fh[1] - ((1 - p_heel[i]) * (p * fm1[1] + (1 - p) * fm5[1]))
+        objective += dot(jf2, jf2) + dot(jf3, jf3) + dot(jf32, jf32) + dot(jf4, jf4)
         # use of p to dispatch forces --> p_heel*Fh - (1-p_heel)*Fm = 0
         jm2 = p_heel[i] * (mh + dot(coord[2], fh)) - ((1 - p_heel[i]) * (p * (mm1 + dot(coord[0], fm1)) + (1 - p) * (mm5 + dot(coord[1], fm5))))
         objective += mtimes(jm2.T, jm2)
@@ -133,23 +210,29 @@ def prepare_ocp(
     objective_functions = (
         (
             {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 1, "controls_idx": range(6, nb_tau)},
-            {"type": Objective.Lagrange.TRACK_MUSCLES_CONTROL, "weight": 0.1, "data_to_track": excitation_ref[0][:, :-1].T,},
+            {"type": Objective.Lagrange.TRACK_MUSCLES_CONTROL, "weight": 1, "data_to_track": excitation_ref[0][:, :-1].T,},
             {"type": Objective.Lagrange.TRACK_MARKERS, "weight": 100, "data_to_track": markers_ref[0]},
-            # {"type": Objective.Lagrange.TRACK_STATE, "weight": 0.01, "states_idx": [0, 1, 5, 8, 9, 11], "data_to_track": q_ref.T},
             {"type": Objective.Lagrange.TRACK_CONTACT_FORCES, "weight": 0.00005, "data_to_track": grf_ref[0].T},
         ),
         (
             {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 1, "controls_idx": range(6, nb_tau)},
-            {"type": Objective.Lagrange.TRACK_MUSCLES_CONTROL, "weight": 0.1,
+            {"type": Objective.Lagrange.TRACK_MUSCLES_CONTROL, "weight": 1,
              "data_to_track": excitation_ref[1][:, :-1].T, },
             {"type": Objective.Lagrange.TRACK_MARKERS, "weight": 100, "data_to_track": markers_ref[1]},
-            # {"type": Objective.Lagrange.TRACK_STATE, "weight": 0.01, "states_idx": [0, 1, 5, 8, 9, 11], "data_to_track": q_ref.T},
             {"type": Objective.Lagrange.TRACK_CONTACT_FORCES, "weight": 0.00005, "data_to_track": grf_ref[1].T},
+        ),
+        (
+            {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 1, "controls_idx": range(6, nb_tau)},
+            {"type": Objective.Lagrange.TRACK_MUSCLES_CONTROL, "weight": 1,
+             "data_to_track": excitation_ref[2][:, :-1].T, },
+            {"type": Objective.Lagrange.TRACK_MARKERS, "weight": 100, "data_to_track": markers_ref[2]},
+            {"type": Objective.Lagrange.TRACK_CONTACT_FORCES, "weight": 0.00005, "data_to_track": grf_ref[2].T},
         ),
     )
 
     # Dynamics
     problem_type = (
+        ProblemType.muscle_excitations_and_torque_driven_with_contact,
         ProblemType.muscle_excitations_and_torque_driven_with_contact,
         ProblemType.muscle_excitations_and_torque_driven_with_contact,
     )
@@ -158,7 +241,7 @@ def prepare_ocp(
     constraints = ({"type": Constraint.CUSTOM, "function": get_muscles_first_node, "instant": Instant.START},)
 
     # State Transitions
-    state_transitions = ({"type": StateTransition.IMPACT, "phase_pre_idx": 0, }, )
+    state_transitions = ({"type": StateTransition.IMPACT, "phase_pre_idx": 0, },)
 
     # Define the parameter to optimize
     bound_length = Bounds(min_bound=np.repeat(0.2, nb_mus), max_bound=np.repeat(5, nb_mus), interpolation_type=InterpolationType.CONSTANT)
@@ -210,11 +293,11 @@ def prepare_ocp(
         final_time,
         X_init,
         U_init,
-        X_bounds=(X_bounds, X_bounds),
-        U_bounds=(U_bounds, U_bounds),
+        X_bounds=(X_bounds, X_bounds, X_bounds),
+        U_bounds=(U_bounds, U_bounds, U_bounds),
         objective_functions=objective_functions,
-        constraints=(constraints, ()),
-        parameters=(parameters, parameters),
+        constraints=(constraints, (), ()),
+        parameters=(parameters, parameters, parameters),
         state_transitions=state_transitions,
     )
 
@@ -229,7 +312,7 @@ if __name__ == "__main__":
   )
 
     # Problem parameters
-    number_shooting_points = [5, 10, 15]
+    number_shooting_points = [10, 5, 25]
 
     # Generate data from file
     Data_to_track = Data_to_track("normal01", multiple_contact=True)
@@ -241,7 +324,6 @@ if __name__ == "__main__":
     markers_ref = Data_to_track.load_data_markers(biorbd_model[0], T_stance, number_shooting_points, "stance")
     q_ref = Data_to_track.load_data_q(biorbd_model[0], T_stance, number_shooting_points, "stance")
     emg_ref = Data_to_track.load_data_emg(biorbd_model[0], T_stance, number_shooting_points, "stance")
-
     excitation_ref = []
     for i in range(len(phase_time)):
         excitation_ref.append(Data_to_track.load_muscularExcitation(emg_ref[i]))
@@ -249,20 +331,18 @@ if __name__ == "__main__":
     Heel = np.array([np.mean(markers_ref[1][0, 19, :] + 0.04), np.mean(markers_ref[1][1, 19, :]), 0])
     Meta1 = np.array([np.mean(markers_ref[1][0, 21, :]), np.mean(markers_ref[1][1, 21, :]), 0])
     Meta5 = np.array([np.mean(markers_ref[1][0, 24, :]), np.mean(markers_ref[1][1, 24, :]), 0])
-    grf_dispatch_ref = get_dispatch_contact_forces(grf_ref[1], M_ref[1], [Meta1, Meta5, Heel], number_shooting_points[1])
-    grf_dispatch_ref = grf_dispatch_ref[[2, 5, 8], :]
+    grf_flatfoot_ref = get_dispatch_contact_forces(grf_ref[1], M_ref[1], [Meta1, Meta5, Heel], number_shooting_points[1])
+    grf_flatfoot_ref = grf_flatfoot_ref[[0, 2, 3, 5, 8], :]
+    grf_forefoot_ref = get_dispatch_forefoot_contact_forces(grf_ref[2], M_ref[2], [Meta1, Meta5], number_shooting_points[2])
+    grf_forefoot_ref = grf_forefoot_ref[[0, 2, 3, 5], :]
 
-    plt.figure()
-    plt.plot(grf_ref[1][2, :].T, 'k--')
-    plt.plot(grf_dispatch_ref.T)
-    plt.legend(('platform','heel', 'Meta1', 'Meta5'))
-    plt.show()
-
-    Q_ref = np.zeros((biorbd_model[1].nbQ(), number_shooting_points[1] + 1))
-    Q_ref[[0, 1, 5, 8, 9, 11],:] = q_ref[1]
 
     Q_ref_0 = np.zeros((biorbd_model[0].nbQ(), number_shooting_points[0] + 1))
     Q_ref_0 [[0, 1, 5, 8, 9, 11],:] = q_ref[0]
+    Q_ref_1 = np.zeros((biorbd_model[1].nbQ(), number_shooting_points[1] + 1))
+    Q_ref_1[[0, 1, 5, 8, 9, 11],:] = q_ref[1]
+    Q_ref_2 = np.zeros((biorbd_model[2].nbQ(), number_shooting_points[2] + 1))
+    Q_ref_2[[0, 1, 5, 8, 9, 11],:] = q_ref[2]
 
     # Get initial isometric forces
     fiso_init = []
@@ -272,13 +352,13 @@ if __name__ == "__main__":
             fiso_init.append(biorbd_model[2].muscleGroup(nGrp).muscle(nMus).characteristics().forceIsoMax().to_mx())
 
     ocp = prepare_ocp(
-        biorbd_model=(biorbd_model[0], biorbd_model[1]),
-        final_time=(phase_time[0], phase_time[1]),
-        nb_shooting=(number_shooting_points[0], number_shooting_points[1]),
-        markers_ref=(markers_ref[0], markers_ref[1]),
-        excitation_ref=(excitation_ref[0], excitation_ref[1]),
-        grf_ref=(grf_ref[0][2, :], grf_dispatch_ref),
-        q_ref=(Q_ref_0, Q_ref),
+        biorbd_model,
+        final_time=phase_time,
+        nb_shooting=number_shooting_points,
+        markers_ref=markers_ref,
+        excitation_ref=excitation_ref,
+        grf_ref=(grf_ref[0][[0, 2], :], grf_flatfoot_ref, grf_forefoot_ref),
+        q_ref=(Q_ref_0, Q_ref_1, Q_ref_2),
         fiso_init=fiso_init,
     )
 
@@ -297,12 +377,20 @@ if __name__ == "__main__":
     )
 
     # --- Get Results --- #
-    states_sol, controls_sol = Data.get_data(ocp, sol["x"])
+    states_sol, controls_sol, params = Data.get_data(ocp, sol["x"], get_parameters=True)
     q = states_sol["q"]
     q_dot = states_sol["q_dot"]
     activations = states_sol["muscles"]
     tau = controls_sol["tau"]
     excitations = controls_sol["muscles"]
+
+    # --- Save Results --- #
+    np.save('excitations', excitations)
+    np.save('activations', activations)
+    np.save('tau', tau)
+    np.save('q_dot', q_dot)
+    np.save('q', q)
+    np.save('params', params[ocp.nlp[0]['p'].name()])
 
     # --- Show results --- #
     result = ShowResult(ocp, sol)
