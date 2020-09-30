79834d18d95aa4feac0ec6c19a5210c158465159
Lea Sanchez <lea.sanchez@umontreal.ca>
2020-09-30 09:33:55 -0400

    [WIP] MOCO example

--- Marche_BiorbdOptim/moco/marche_moco_multiphase.py
+++ Marche_BiorbdOptim/moco/marche_moco_multiphase.py
@@ -3,8 +3,9 @@ from casadi import dot, Function, vertcat, MX
 from matplotlib import pyplot as plt
 import biorbd
 from time import time
-from Marche_BiorbdOptim.moco.Load_OpenSim_data import get_q, get_grf
+import Marche_BiorbdOptim.moco.Load_OpenSim_data as Moco
 import Marche_BiorbdOptim.moco.constraints_dof as Constraints
+from BiorbdViz import BiorbdViz
 
 from biorbd_optim import (
     OptimalControlProgram,
@@ -26,12 +27,13 @@ from biorbd_optim import (
     StateTransitionList,
     StateTransition,
     Solver,
+    Simulate,
 )
 
 
 
 def prepare_ocp(
-    biorbd_model, phase_time, number_shooting_points, grf_ref, q_ref, qdot_ref,
+    biorbd_model, phase_time, number_shooting_points, q_ref, qdot_ref, tau_ref, nb_thread,
 ):
     # Problem parameters
     nb_phases = len(biorbd_model)
@@ -44,8 +46,11 @@ def prepare_ocp(
     # Add objective functions
     objective_functions = ObjectiveList()
     for p in range(nb_phases):
-        objective_functions.add(Objective.Lagrange.TRACK_STATE, weight=100, data_to_track=q_ref[p].T, states_idx=range(nb_q), phase=p)
-        objective_functions.add(Objective.Lagrange.MINIMIZE_TORQUE, weight=10, phase=p)
+        q_tracked = np.zeros((2*nb_q, number_shooting_points[p] + 1))
+        q_tracked[:nb_q, :] = q_ref[p]
+        q_tracked[nb_q:, :] = qdot_ref[p]
+        # objective_functions.add(Objective.Lagrange.TRACK_STATE, weight=100, target=q_tracked,  phase=p)
+        objective_functions.add(Objective.Lagrange.MINIMIZE_ALL_CONTROLS, weight=1, phase=p)
 
     # Dynamics
     dynamics = DynamicsTypeList()
@@ -53,7 +58,40 @@ def prepare_ocp(
         dynamics.add(DynamicsType.TORQUE_DRIVEN_WITH_CONTACT, phase=p)
 
     # Constraints
+    x = np.array(
+        [0, 0.174533, 0.349066, 0.523599, 0.698132, 0.872665, 1.0472, 1.22173, 1.39626, 1.5708, 1.74533, 1.91986,
+         2.0944])
     constraints = ConstraintList()
+    for p in range(nb_phases):
+        # # -- tibia_r --
+        # constraints.add(Constraints.tibia_r_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=9)
+        # constraints.add(Constraints.tibia_r_Tz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=10)
+        # constraints.add(Constraints.tibia_r_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=12)
+        # constraints.add(Constraints.tibia_r_Ry, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=13)
+        #
+        # # -- patella_r --
+        # constraints.add(Constraints.patella_r_Tx, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=14)
+        # constraints.add(Constraints.patella_r_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=15)
+        # constraints.add(Constraints.patella_r_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=16)
+        #
+        # # -- tibia_l --
+        # constraints.add(Constraints.tibia_l_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=21)
+        # constraints.add(Constraints.tibia_l_Tz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=22)
+        # constraints.add(Constraints.tibia_l_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=24)
+        # constraints.add(Constraints.tibia_l_Ry, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=25)
+        #
+        # # -- patella_l --
+        # constraints.add(Constraints.patella_l_Tx, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=26)
+        # constraints.add(Constraints.patella_l_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=27)
+        # constraints.add(Constraints.patella_l_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=28)
+
+        # -- track state --
+        constraints.add(
+            Constraint.TRACK_STATE,
+            instant=Instant.ALL,
+            states_idx=range(nb_q),
+            target=q_ref[p],
+        )
 
     # Path constraint
     x_bounds = BoundsList()
@@ -69,13 +107,26 @@ def prepare_ocp(
 
     x_init = InitialConditionsList()
     for p in range(nb_phases):
+        # if (p==0):
+        #     init_x = np.zeros((nb_q + nb_qdot, number_shooting_points[p] + 1))
+        #     init_x[:nb_q, :] = np.load("./RES/part0/q.npy")
+        #     init_x[nb_q: nb_q + nb_qdot, :] = np.load("./RES/part0/q_dot.npy")
+        #     x_init.add(init_x, interpolation=InterpolationType.EACH_FRAME)
+        # else:
         init_x = np.zeros((nb_q + nb_qdot, number_shooting_points[p] + 1))
-        init_x[:nb_q, :] = q_ref[p]
-        init_x[nb_q : nb_q + nb_qdot, :] = qdot_ref[p]
+        init_x[:nb_q, :] = np.load("./RES/part0/q.npy")
+        init_x[nb_q: nb_q + nb_qdot, :] = np.load("./RES/part0/q_dot.npy")
+        # init_x[:nb_q, :] = q_ref[p]
+        # init_x[nb_q: nb_q + nb_qdot, :] = qdot_ref[p]
         x_init.add(init_x, interpolation=InterpolationType.EACH_FRAME)
 
     u_init = InitialConditionsList()
     for p in range(nb_phases):
+        # if (p==0):
+        #     init_u = np.load("./RES/part0/tau.npy")[:, :-1]
+        #     u_init.add(init_u, interpolation=InterpolationType.EACH_FRAME)
+        # else:
+        # init_u = tau_ref[p]
         init_u = np.zeros((nb_tau, number_shooting_points[p]))
         u_init.add(init_u, interpolation=InterpolationType.EACH_FRAME)
 
@@ -123,46 +174,57 @@ if __name__ == "__main__":
     final_time = t_end - t_init
     nb_shooting = 50
 
-    [Q_ref, Qdot_ref, Qddot_ref] = get_q(t_init, t_end, final_time, nb_q, nb_shooting)
-    GRF_ref = get_grf(t_init, t_end, final_time, nb_shooting)
-    t = np.linspace(0, final_time, nb_shooting + 1)
+    # Generate data from file OpenSim
+    [Q_ref, Qdot_ref, Qddot_ref] = Moco.get_state_tracked(t_init, t_end, final_time, nb_q, nb_shooting)
+    [Q_sol, Qdot_sol, Activation_sol] = Moco.get_state_from_solution(t_init, t_end, final_time, nb_q, nb_shooting)
+    [Tau_sol, Excitation_sol] = Moco.get_control_from_solution(t_init, t_end, final_time, nb_q, nb_shooting)
+    Tau_ref = Moco.get_tau_from_inverse_dynamics(t_init, t_end, final_time, nb_q, nb_shooting, Qddot_ref)
 
     # q_ref = (Q_ref[:, :7], Q_ref[:, 6:16], Q_ref[:, 15:25], Q_ref[:, 24:35])
     # qdot_ref = (Qdot_ref[:, :7], Qdot_ref[:, 6:16], Qdot_ref[:, 15:25], Qdot_ref[:, 24:35])
+    #
+    # biorbd_model = [biorbd.Model("../../ModelesS2M/Open_Sim/Multiphase/subject_walk_armless_test_1.bioMod"),
+    #                 biorbd.Model("../../ModelesS2M/Open_Sim/Multiphase/subject_walk_armless_test_2.bioMod"),]
+    # q_ref = [Q_ref[:, :7], Q_ref[:, 6:16],]
+    # qdot_ref = [Qdot_ref[:, :7], Qdot_ref[:, 6:16],]
+    # tau_ref = [Tau_ref[:, :6], Tau_ref[:, 6:15]]
+    # number_shooting_points = [6, 9]
+    # phase_time = [0.1008, 0.1512]
 
-    q_ref = (Q_ref[:, :7])
-    qdot_ref = (Qdot_ref[:, :7])
-    number_shooting_points = [6]
-    phase_time = [0.1008]
+    biorbd_model = [biorbd.Model("../../ModelesS2M/Open_Sim/Multiphase/subject_walk_armless_test_1.bioMod"),]
+    q_ref = [Q_ref[:, :7],]
+    qdot_ref = [Qdot_ref[:, :7],]
+    tau_ref = [Tau_ref[:, :7],]
+    number_shooting_points = [6,]
+    phase_time = [0.1008,]
 
     # plt.plot(t, GRF_ref[1, :])
     # plt.plot(t, GRF_ref[7, :])
     # plt.legend(["right", "left"])
 
-    # Track these data
-    biorbd_model = (
-        biorbd.Model("../../ModelesS2M/Open_Sim/Multiphase/subject_walk_armless_test_1.bioMod"),
-        # biorbd.Model("../../ModelesS2M/Open_Sim/Multiphase/subject_walk_armless_test_2.bioMod"),
-        # biorbd.Model("../../ModelesS2M/Open_Sim/Multiphase/subject_walk_armless_test_3.bioMod"),
-        # biorbd.Model("../../ModelesS2M/Open_Sim/Multiphase/subject_walk_armless_test_4.bioMod"),
-    )
-
     ocp = prepare_ocp(
         biorbd_model=biorbd_model,
         phase_time=phase_time,
         number_shooting_points=number_shooting_points,
-        grf_ref=GRF_ref,
         q_ref=q_ref,
         qdot_ref=qdot_ref,
+        tau_ref=tau_ref,
+        nb_thread=4,
     )
+
+    # sim = Simulate.from_controls_and_initial_states(ocp, ocp.original_values["X_init"][0], ocp.original_values["U_init"][0], single_shoot=True)
+    # states_sim, controls_sim = Data.get_data(ocp, sim["x"])
+    # ShowResult(ocp, sim).graphs()
+    # ShowResult(ocp, sim).animate()
+
 
     # --- Solve the program --- #
     tic = time()
     sol = ocp.solve(
         solver=Solver.IPOPT,
         solver_options={
-            "ipopt.tol": 1e-2,
-            "ipopt.max_iter": 5000,
+            "ipopt.tol": 1e-4,
+            "ipopt.max_iter": 100,
             "ipopt.hessian_approximation": "exact",
             "ipopt.limited_memory_max_history": 50,
             "ipopt.linear_solver": "ma57",
@@ -173,13 +235,37 @@ if __name__ == "__main__":
     print(f"Time to solve : {toc}sec")
 
     # --- Get Results --- #
-    states_sol, controls_sol, params_sol = Data.get_data(ocp, sol["x"], get_parameters=True)
+    states_sol, controls_sol = Data.get_data(ocp, sol["x"])
     q = states_sol["q"]
     q_dot = states_sol["q_dot"]
     tau = controls_sol["tau"]
-    params = params_sol[ocp.nlp[0]["p"].name()]
+
+    # --- Save Results --- #
+    np.save("./RES/part0/tau", tau)
+    np.save("./RES/part0/q_dot", q_dot)
+    np.save("./RES/part0/q", q)
+
+    q_name = []
+    for s in range(biorbd_model[0].nbSegment()):
+        seg_name = biorbd_model[0].segment(s).name().to_string()
+        for d in range(biorbd_model[0].segment(s).nbDof()):
+            dof_name = biorbd_model[0].segment(s).nameDof(d).to_string()
+            q_name.append(seg_name + "_" + dof_name)
+
+    figure, axes = plt.subplots(5, 6)
+    axes = axes.flatten()
+    for i in range(nb_q):
+        axes[i].plot(q_ref[0][i, :], "b")
+        axes[i].plot(q[i, :], "r")
+        # axes[i].set_ylim([np.min(q_ref[0][i, :]) - 0.05, np.max(q_ref[0][i, :]) + 0.05])
+        axes[i].set_title(q_name[i])
+
 
     # --- Show results --- #
+    b = BiorbdViz(loaded_model=biorbd_model[0])
+    # b.load_movement(q_ref[0])
+    b.load_movement(q)
+
     result = ShowResult(ocp, sol)
     result.animate()
     result.graphs()
--- Marche_BiorbdOptim/moco/marche_moco_torque_driven.py
+++ Marche_BiorbdOptim/moco/marche_moco_torque_driven.py
@@ -2,84 +2,86 @@ import numpy as np
 import biorbd
 from time import time
 from BiorbdViz import BiorbdViz
-from casadi import vertcat, MX, Function, interp1d, interpolant
+from casadi import vertcat, MX, Function, jacobian
 from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
-from Marche_BiorbdOptim.moco.Load_OpenSim_data import get_q, get_grf, get_position
+import Marche_BiorbdOptim.moco.Load_OpenSim_data as Moco
 import Marche_BiorbdOptim.moco.constraints_dof as Constraints
 
 from biorbd_optim import (
     OptimalControlProgram,
-    DynamicsTypeList,
-    DynamicsType,
     BoundsList,
-    Bounds,
+    Constraint,
+    ConstraintList,
+    DynamicsType,
+    DynamicsTypeList,
     QAndQDotBounds,
     InitialConditionsList,
-    InitialConditions,
     ShowResult,
-    ObjectiveList,
-    Objective,
-    InterpolationType,
     Data,
+    Objective,
+    ObjectiveList,
+    InterpolationType,
     Solver,
-    ConstraintList,
-    Instant,
     Simulate,
+    OdeSolver,
 )
 
+def plot_control(ax, t, x, color="k", linestyle="--", linewidth=0.7):
+    nbPoints = len(np.array(x))
+    for n in range(nbPoints - 1):
+        ax.plot([t[n], t[n + 1], t[n + 1]], [x[n], x[n], x[n + 1]], color, linestyle, linewidth)
 
 def prepare_ocp(
-    biorbd_model, final_time, nb_shooting, q_ref, qdot_ref, grf_ref, moment_ref, nb_threads,
+    biorbd_model, final_time, nb_shooting, q_ref, qdot_ref, tau_ref, activation_ref, grf_ref, moment_ref, nb_threads,
 ):
     # Problem parameters
     nb_q = biorbd_model.nbQ()
     nb_qdot = biorbd_model.nbQdot()
     nb_tau = biorbd_model.nbGeneralizedTorque()
-
-    torque_min, torque_max, torque_init = -1000, 1000, 0
+    torque_min, torque_max = -1500, 1500
 
     # Add objective functions
     objective_functions = ObjectiveList()
-    objective_functions.add(Objective.Lagrange.MINIMIZE_TORQUE, weight=1, phase=0)
-    objective_functions.add(Objective.Lagrange.TRACK_STATE, weight=100, states_idx=range(nb_q), data_to_track=q_ref.T, phase=0)
+    #objective_functions.add(Objective.Lagrange.MINIMIZE_TORQUE, controls_idx=range(6, nb_q), weight=0.01, phase=0)
+    objective_functions.add(Objective.Lagrange.TRACK_STATE, weight=100, states_idx=range(nb_q), target=q_ref, phase=0)
 
     # Dynamics
     dynamics = DynamicsTypeList()
     dynamics.add(DynamicsType.TORQUE_DRIVEN, phase=0)
 
     # Constraints
-    x = np.array(
-        [0, 0.174533, 0.349066, 0.523599, 0.698132, 0.872665, 1.0472, 1.22173, 1.39626, 1.5708, 1.74533, 1.91986,
-         2.0944])
+    # x = np.array(
+    #     [0, 0.174533, 0.349066, 0.523599, 0.698132, 0.872665, 1.0472, 1.22173, 1.39626, 1.5708, 1.74533, 1.91986,
+    #      2.0944])
     constraints = ConstraintList()
-    # -- tibia_r --
-    constraints.add(Constraints.tibia_r_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=9)
-    constraints.add(Constraints.tibia_r_Tz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=10)
-    constraints.add(Constraints.tibia_r_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=12)
-    constraints.add(Constraints.tibia_r_Ry, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=13)
-
-    # -- patella_r --
-    constraints.add(Constraints.patella_r_Tx, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=14)
-    constraints.add(Constraints.patella_r_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=15)
-    constraints.add(Constraints.patella_r_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=16)
-
-    # -- tibia_l --
-    constraints.add(Constraints.tibia_l_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=21)
-    constraints.add(Constraints.tibia_l_Tz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=22)
-    constraints.add(Constraints.tibia_l_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=24)
-    constraints.add(Constraints.tibia_l_Ry, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=25)
-
-    # -- patella_l --
-    constraints.add(Constraints.patella_l_Tx, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=26)
-    constraints.add(Constraints.patella_l_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=27)
-    constraints.add(Constraints.patella_l_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=28)
+    # # -- tibia_r --
+    # constraints.add(Constraints.tibia_r_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=9)
+    # constraints.add(Constraints.tibia_r_Tz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=10)
+    # constraints.add(Constraints.tibia_r_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=12)
+    # constraints.add(Constraints.tibia_r_Ry, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=13)
+    #
+    # # -- patella_r --
+    # constraints.add(Constraints.patella_r_Tx, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=14)
+    # constraints.add(Constraints.patella_r_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=15)
+    # constraints.add(Constraints.patella_r_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=11, q_target_idx=16)
+    #
+    # # -- tibia_l --
+    # constraints.add(Constraints.tibia_l_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=21)
+    # constraints.add(Constraints.tibia_l_Tz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=22)
+    # constraints.add(Constraints.tibia_l_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=24)
+    # constraints.add(Constraints.tibia_l_Ry, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=25)
+    #
+    # # -- patella_l --
+    # constraints.add(Constraints.patella_l_Tx, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=26)
+    # constraints.add(Constraints.patella_l_Ty, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=27)
+    # constraints.add(Constraints.patella_l_Rz, instant=Instant.ALL, x_interpol=x, q_ref_idx=23, q_target_idx=28)
 
     # External forces
     external_forces = [np.zeros((6, 2, nb_shooting))] # 1 torseur par jambe
     for i in range(len(grf_ref)):
-        external_forces[0][:3, i, :] = grf_ref[i][:, :-1]
-        external_forces[0][3:, i, :] = moment_ref[i][:, :-1]
+        external_forces[0][:3, i, :] = moment_ref[i][:, :-1]
+        external_forces[0][3:, i, :] = grf_ref[i][:, :-1]
 
     # Path constraint
     x_bounds = BoundsList()
@@ -100,22 +102,25 @@ def prepare_ocp(
 
     u_init = InitialConditionsList()
     init_u = np.zeros((nb_tau, nb_shooting))
+    init_u[:nb_tau] = tau_ref
     u_init.add(init_u, interpolation=InterpolationType.EACH_FRAME)
 
     # ------------- #
 
     return OptimalControlProgram(
-        biorbd_model,
-        dynamics,
-        nb_shooting,
-        final_time,
-        x_init,
-        u_init,
-        x_bounds,
-        u_bounds,
-        objective_functions,
+        biorbd_model=biorbd_model,
+        dynamics_type=dynamics,
+        number_shooting_points=nb_shooting,
+        phase_time=final_time,
+        X_init=x_init,
+        U_init=u_init,
+        X_bounds=x_bounds,
+        U_bounds=u_bounds,
+        objective_functions=objective_functions,
         constraints=constraints,
+        nb_threads=nb_threads,
         external_forces=external_forces,
+        ode_solver=OdeSolver.RK,
     )
 
 
@@ -123,47 +128,35 @@ if __name__ == "__main__":
     # Define the problem
     biorbd_model = biorbd.Model("../../ModelesS2M/Open_Sim/subject_walk_armless_test.bioMod")
     t_init = 0.81
-    t_end = 1.65
-    final_time = t_end - t_init
-    nb_shooting = 50
+    t_end = 1.7
+    dt = 0.01
+    final_time = np.round(t_end - t_init, 2)
+    nb_shooting = int(final_time/dt)
 
     # model parameters
     nb_q = biorbd_model.nbQ()
     nb_qdot = biorbd_model.nbQdot()
     nb_tau = biorbd_model.nbGeneralizedTorque()
+    nb_mus = biorbd_model.nbMuscleTotal()
     nb_markers = biorbd_model.nbMarkers()
     node_t = np.linspace(0, final_time, nb_shooting + 1)
 
     # Generate data from file OpenSim
-    [Q_ref, Qdot_ref, Qddot_ref] = get_q(t_init, t_end, final_time, biorbd_model.nbQ(), nb_shooting)
-    [Force_ref, Moment_ref] = get_grf(t_init, t_end, final_time, nb_shooting)
-    position = get_position(t_init, t_end, final_time, nb_shooting)
+    [Q_ref, Qdot_ref, Qddot_ref] = Moco.get_state_tracked(t_init, t_end, final_time, nb_q, nb_shooting)
+    [Q_MI, Qdot_MI, Activation_MI] = Moco.get_state_from_solution_MI(t_init, t_end, final_time, nb_q, nb_shooting)
+    [Tau_MI, Excitation_MI] = Moco.get_control_from_solution_MI(t_init, t_end, final_time, nb_q, nb_shooting)
+    [Force_ref, Moment_ref] = Moco.get_grf(t_init, t_end, final_time, nb_shooting)
+    position = Moco.get_position(t_init, t_end, final_time, nb_shooting)
+    Tau_OpenSim_ID = Moco.get_tau_from_inverse_dynamics("inverse_dynamics_fext.xlsx", t_init, t_end, final_time, nb_q, nb_shooting)
 
-    symbolic_q = MX.sym("x", nb_q, 1)
-    markers_func=Function(
-        "ForwardKin",
-        [symbolic_q],
-        [biorbd_model.markers(symbolic_q)],
-        ["q"],
-        ["marker_pos"],
-        ).expand()
-
+    # Compute ankle moments
     markers_pos = np.zeros((3, nb_markers, nb_shooting + 1))
     for i in range(nb_shooting + 1):
-        markers_pos[:, :, i]=markers_func(Q_ref[:, i])
-
-    # b = BiorbdViz(loaded_model=biorbd_model)
-    # b.load_movement(Q_ref)
-
-    fig = plt.figure()
-    ax = plt.axes(projection="3d")
-    ax.scatter3D(position[0][0, :], position[0][1, :], position[0][2, :], color='blue')
-    ax.scatter3D(position[1][0, :], position[1][1, :], position[1][2, :], color='red')
-    ax.scatter3D(markers_pos[0, 0, :], markers_pos[1, 0, :], markers_pos[2, 0, :], color='green')
-    ax.scatter3D(markers_pos[0, 1, :], markers_pos[1, 1, :], markers_pos[2, 1, :], color='magenta')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+        for m in range(nb_markers):
+            # --- define casadi function ---
+            q_iv = MX.sym("Q", nb_q, 1)
+            marker_func = biorbd.to_casadi_func("marker", biorbd_model.marker, q_iv, m)
+            markers_pos[:, m, i:i + 1] = marker_func(Q_ref[:, i])
 
     Mext = []
     for leg in range(len(Force_ref)):
@@ -173,54 +166,75 @@ if __name__ == "__main__":
         marker = markers_pos[:, leg, :]
         M = np.zeros((3, nb_shooting + 1))
         for i in range(nb_shooting + 1):
-            p = pos[:, i] - marker[:, i]
-            M[:, i]=np.cross(p, force[:, i]) + moment[:, i]
+            p = marker[:, i] - pos[:, i]
+            M[:, i] = np.cross(p, force[:, i]) + moment[:, i]
         Mext.append(M)
 
-    figure, axes = plt.subplots(1, 3)
-    axes[0].plot(node_t, Force_ref[0].T)
-    axes[0].set_title('Forces right')
-    axes[1].plot(node_t, Moment_ref[0].T)
-    axes[1].set_title('Moments init right')
-    axes[2].plot(node_t, Mext[0].T)
-    axes[2].set_title('Moments ankle right')
-    plt.legend(['x', 'y', 'z'])
+    # Use ID to compute initial guess
+    Tau_ID = np.zeros((nb_q, nb_shooting + 1))
+    for n in range(nb_shooting + 1):
+        # --- define external forces ---
+        forces = biorbd.VecBiorbdSpatialVector()
+        forces.append(biorbd.SpatialVector(MX(
+            (Mext[0][0, n], Mext[0][1, n], Mext[0][2, n], Force_ref[0][0, n], Force_ref[0][1, n], Force_ref[0][2, n]))))
+        forces.append(biorbd.SpatialVector(MX(
+            (Mext[1][0, n], Mext[1][1, n], Mext[1][2, n], Force_ref[1][0, n], Force_ref[1][1, n], Force_ref[1][2, n]))))
+        # --- define casadi function ---
+        q_iv = MX.sym("Q", nb_q, 1)
+        dq_iv = MX.sym("Qdot", nb_q, 1)
+        ddq_iv = MX.sym("Qddot", nb_q, 1)
+        # func = biorbd.to_casadi_func("ID", biorbd_model.InverseDynamics, q_iv, dq_iv, ddq_iv, forces)
+        func = biorbd.to_casadi_func("ID", biorbd_model.InverseDynamics, q_iv, dq_iv, ddq_iv)
+        # --- compute torques from inverse dynamics ---
+        q_iv = Q_ref[:, n]
+        dq_iv = Qdot_ref[:, n]
+        ddq_iv = Qddot_ref[:, n]
+        Tau_ID[:, n:n + 1] = func(q_iv, dq_iv, ddq_iv)
+    #
+    # # --- Show results --- #
+    # b = BiorbdViz(loaded_model=biorbd_model)
+    # b.load_movement(Q_ref)
+    # b.exec()
 
-    figure, axes = plt.subplots(1, 3)
-    axes[0].plot(node_t, Force_ref[1].T)
-    axes[0].set_title('Forces left')
-    axes[1].plot(node_t, Moment_ref[1].T)
-    axes[1].set_title('Moments init left')
-    axes[2].plot(node_t, Mext[1].T)
-    axes[2].set_title('Moments ankle left')
-    plt.legend(['x', 'y', 'z'])
-
-    plt.show()
     # Track these data
     ocp = prepare_ocp(
-        biorbd_model,
+        biorbd_model=biorbd_model,
         final_time=final_time,
         nb_shooting=nb_shooting,
         q_ref=Q_ref,
         qdot_ref=Qdot_ref,
+        tau_ref=Tau_ID[:, :-1],
+        activation_ref=Activation_MI[:, :-1],
         grf_ref=Force_ref,
         moment_ref=Mext,
         nb_threads=1,
     )
 
-    # U_init_sim = InitialConditionsList()
-    # U_init_sim.add([0]*nb_tau, interpolation=InterpolationType.CONSTANT)
-    # sim = Simulate.from_controls_and_initial_states(ocp, ocp.original_values["X_init"][0], U_init_sim[0], single_shoot=True)
-    # states_sim, controls_sim = Data.get_data(ocp, sim["x"])
-    # ShowResult(ocp, sim).graphs()
-    # ShowResult(ocp, sim).animate()
+    # # --- Nan in initial guess ? --- #
+    # # compute constraint value + jacobian
+    # g = Function("g", [ocp.V], ocp.g[0])
+    # Jg_init = []
+    # for i in range(nb_shooting):
+    #     Jg = Function("Jg", [ocp.V], [jacobian(ocp.g[0][i], ocp.V)])
+    #     Jg_init.append(Jg(ocp.V_init.init))
+    # g_init = g(ocp.V_init.init)
+    #
+    # Nan_g = []
+    # Nan_Jg = []
+    # for i in range(nb_shooting):
+    #     Nan_Jg.append(np.isnan(np.array(Jg_init[0])).sum())
+    #     Nan_g.append(np.isnan(np.array(g_init[0])).sum())
+
+    sim = Simulate.from_controls_and_initial_states(ocp, ocp.original_values["X_init"][0], ocp.original_values["U_init"][0], single_shoot=True)
+    states_zeros, controls_zeros = Data.get_data(ocp, sim["x"])
+    ShowResult(ocp, sim).graphs()
 
     # --- Solve the program --- #
     tic = time()
     sol = ocp.solve(
         solver=Solver.IPOPT,
         solver_options={
-            "ipopt.tol": 1e-3,
+            "ipopt.tol": 1e-2,
             "ipopt.max_iter": 5000,
             "ipopt.hessian_approximation": "exact",
             "ipopt.limited_memory_max_history": 50,
@@ -232,10 +246,11 @@ if __name__ == "__main__":
     print(f"Time to solve : {toc}sec")
 
     # --- Get Results --- #
-    states_sol, controls_sol, params_sol = Data.get_data(ocp, sol["x"], get_parameters=True)
+    states_sol, controls_sol = Data.get_data(ocp, sol["x"])
     q = states_sol["q"]
     q_dot = states_sol["q_dot"]
     tau = controls_sol["tau"]
 
     # --- Show results --- #
     ShowResult(ocp, sol).animate()
+    ShowResult(ocp, sol).graphs()
