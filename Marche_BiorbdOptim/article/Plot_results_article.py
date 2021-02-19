17e9d542ff3de2762e139df95831d7c7f5013621
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-02-19 14:30:41 -0500

    Added plot Iliopsoas

--- Marche_BiorbdOptim/article/Plot_results_article.py
+++ Marche_BiorbdOptim/article/Plot_results_article.py
@@ -36,29 +36,31 @@ def plot_muscles(activations):
 def plot_muscles_2(activations):
     seaborn.color_palette('deep')
     label_muscles = ("Gluteus Maximus",
-                       "Hamstrings",
-                       "Vastus Medialis",
-                       "Rectus Femoris",
-                       "Gastrocnemius Medialis",
-                       "Tibialis Anterior")
+                     "Iliopsoas",
+                     "Hamstrings",
+                     "Vastus Medialis",
+                     "Rectus Femoris",
+                     "Gastrocnemius Medialis",
+                     "Tibialis Anterior")
     ACT_plot = np.zeros(len(label_muscles))
     ACT_plot[0] = np.mean(activations[:3])  # Gluteus Maximus
-    ACT_plot[1] = activations[6]  # Semimembranous
-    ACT_plot[2] = activations[10]  # Vastus Medialis
-    ACT_plot[3] = activations[9]  # Rectus Femoris
-    ACT_plot[4] = activations[13]  # Gastrocnemius Medialis
-    ACT_plot[5] = activations[16]  # Tibial Anterior
+    ACT_plot[1] = np.mean(activations[6:8])
+    ACT_plot[2] = np.mean(activations[8:10])  # Semimembranous
+    ACT_plot[3] = activations[12]  # Vastus Medialis
+    ACT_plot[4] = activations[11]  # Rectus Femoris
+    ACT_plot[5] = activations[15]  # Gastrocnemius Medialis
+    ACT_plot[6] = activations[18]  # Tibial Anterior
 
     fig, ax = plt.subplots()
     for m in range(len(label_muscles)):
         act = np.zeros(len(label_muscles))
         act[m]=ACT_plot[m]
-        plt.bar(np.arange(6), act, width=1.0, edgecolor='k')
-    plt.xticks(np.arange(6), label_muscles, rotation=90)
+        plt.bar(np.arange(7), act, width=1.0, edgecolor='k')
+    plt.xticks(np.arange(7), label_muscles, rotation=90)
     plt.ylabel("Muscle activation")
     plt.yticks(np.arange(0.0, 1.5, 0.5))
     plt.ylim([0.0, 1.0])
-    plt.xlim([-0.5, 5.5])
+    plt.xlim([-0.5, 6.5])
 
 
 # --- Load model and parameters --- #
@@ -68,14 +70,14 @@ nb_mus = model.nbMuscleTotal()
 nb_tau = model.nbGeneralizedTorque()
 
 # --- Load results --- #
-q = np.load("q.npy")
-qdot = np.load("qdot.npy")
-tau = np.load("tau.npy")
-activations = np.load("activation.npy")
+q = np.load("muscle_hip/hip2/q_hip.npy")
+qdot = np.load("muscle_hip/hip2/qdot_hip.npy")
+tau = np.load("muscle_hip/hip2/tau_hip.npy")
+activations = np.load("muscle_hip/hip2/activation_hip.npy")
 
 # --- Plot muscles activity --- #
 
-list_idx = (1,9,53,70,80,90)
+list_idx = (1,9,50,58,75,90)
 for i in list_idx:
     plot_muscles_2(activations[:, i])  # Test 2
 plt.show()
