580cfbf38d26c171bc91e8245f4039ecc8d86d11
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-03-05 09:24:25 -0500

    Added objective symetry for the grf

--- Marche_BiorbdOptim/squat/ocp/objective_functions.py
+++ Marche_BiorbdOptim/squat/ocp/objective_functions.py
@@ -1,6 +1,19 @@
 from bioptim import ObjectiveFcn, Node, PenaltyNodes
 from casadi import MX, vertcat
 
+def sym_forces(pn: PenaltyNodes) -> MX:
+    ns = pn.nlp.ns # number of shooting points
+    nc = pn.nlp.model.nbContacts() # number of contact forces
+    val = []  # init
+
+    # --- compute forces ---
+    for n in range(ns):
+        force = pn.nlp.contact_forces_func(pn.x[n], pn.u[n], pn.p)  # compute force
+        for c in range(int(nc/2)):
+            val = vertcat(val, (force[c]**2 - force[c+int(nc/2)]**2))
+    return val
+
+
 class objective:
     @staticmethod
     def set_objectif_function(objective_functions):
@@ -16,4 +29,9 @@ class objective:
                                 quadratic=True,
                                 node=Node.ALL,
                                 weight=10)
+
+        objective_functions.add(sym_forces,
+                                custom_type=ObjectiveFcn.Lagrange,
+                                node=Node.ALL,
+                                weight=0.1)
         return objective_functions
