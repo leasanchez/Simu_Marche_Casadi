c7bc5ac544834b7098c98c1842704bdb909baf92
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-03-05 09:45:50 -0500

    Used class initial guess for torque driven

--- Marche_BiorbdOptim/squat/Squat.py
+++ Marche_BiorbdOptim/squat/Squat.py
@@ -30,42 +30,8 @@ from ocp.bounds_functions import bounds
 from ocp.initial_guess_functions import initial_guess
 
 
-def custom_CoM_low(pn: PenaltyNodes) -> MX:
-    nq = pn.nlp.shape["q"]
-    compute_CoM = biorbd.to_casadi_func("CoM", pn.nlp.model.CoM, pn.nlp.q)
-    com = compute_CoM(pn.x[15][:nq])
-    return com[2] + 0.30
-
-def custom_CoM_variation(pn: PenaltyNodes) -> MX:
-    nq = pn.nlp.shape["q"]
-    compute_CoM = biorbd.to_casadi_func("CoM", pn.nlp.model.CoM, pn.nlp.q)
-    com_init = compute_CoM(pn.x[0][:nq])
-    val = []
-    for n in range(1, pn.nlp.ns):
-        val = vertcat(val, com_init[0] - compute_CoM(pn.x[pn.t[n]][:nq])[0])
-        val = vertcat(val, com_init[1] - compute_CoM(pn.x[pn.t[n]][:nq])[1])
-    return val
-
-def custom_CoM_high(pn: PenaltyNodes) -> MX:
-    nq = pn.nlp.shape["q"]
-    compute_CoM = biorbd.to_casadi_func("CoM", pn.nlp.model.CoM, pn.nlp.q)
-    com = compute_CoM(pn.x[0][:nq])
-    return com[2]
-
 # OPTIMAL CONTROL PROBLEM
 model = biorbd.Model("Modeles_S2M/2legs_18dof_flatfootR.bioMod")
-# c = model.contactNames()
-# for (i, name) in enumerate(c):
-#     print(f"{i} : {name.to_string()}")
-
-# q_name = []
-# for s in range(model.nbSegment()):
-#     seg_name = model.segment(s).name().to_string()
-#     for d in range(model.segment(s).nbDof()):
-#         dof_name = model.segment(s).nameDof(d).to_string()
-#         q_name.append(seg_name + "_" + dof_name)
-# for (i, q) in enumerate(q_name):
-#     print(f"{i} : {q}")
 
 # --- Problem parameters --- #
 nb_q = model.nbQ()
@@ -74,9 +40,6 @@ nb_tau = model.nbGeneralizedTorque()
 nb_mus = model.nbMuscleTotal()
 nb_shooting = 30
 final_time=1.0
-min_bound, max_bound = 0, np.inf
-torque_min, torque_max, torque_init = -1000, 1000, 0
-activation_min, activation_max, activation_init = 1e-3, 1.0, 0.1
 
 # --- Subject positions and initial trajectories --- #
 position_zeros = [0]*nb_q
@@ -86,15 +49,6 @@ position_high = [[0], [-0.07], [0], [0], [0], [-0.4],
 position_low = [-0.06, -0.36, 0, 0, 0, -0.8,
                 0, 0, 1.53, -1.55, 0, 0.68,
                 0, 0, 1.53, -1.55, 0, 0.68]
-
-# position_high = [[0], [-0.07], [0], [0], [0], [-0.4],
-#                 [0], [0], [0],
-#                 [0], [0], [0.37], [-0.13], [0], [0.11],
-#                 [0], [0], [0.37], [-0.13], [0], [0.11]]
-# position_low = [-0.06, -0.36, 0, 0, 0, -0.8,
-#                 0, 0, 0,
-#                 0, 0, 1.53, -1.55, 0, 0.68,
-#                 0, 0, 1.53, -1.55, 0, 0.68]
 q_init = np.zeros((nb_q, nb_shooting + 1))
 for i in range(nb_q):
     q_init[i, :int(nb_shooting/2)] = np.linspace(position_high[i], position_low[i], int(nb_shooting/2)).squeeze()
@@ -143,26 +97,9 @@ x_bounds, u_bounds = bounds.set_bounds_torque_driven(model, x_bounds, u_bounds, 
 
 # --- Initial guess --- #
 x_init = InitialGuessList()
-init_x = np.zeros((nb_q + nb_qdot, nb_shooting + 1))
-init_x[:nb_q, :] = q_init
-init_x[nb_q:, :] = qdot_init
-x_init.add(init_x, interpolation=InterpolationType.EACH_FRAME)
-
 u_init = InitialGuessList()
-# u_init.add([torque_init]*nb_tau + [activation_init]*nb_mus)
-u_init.add([torque_init] * nb_tau)
-
-# # Load previous solution
-# save_path = './RES/torque_driven/'
-# x_init = InitialGuessList()
-# init_x = np.zeros((nb_q + nb_qdot, nb_shooting + 1))
-# init_x[:nb_q, :] = np.load(save_path + "q.npy")
-# init_x[nb_q:, :] = np.load(save_path + "qdot.npy")
-# x_init.add(init_x, interpolation=InterpolationType.EACH_FRAME)
-#
-# u_init = InitialGuessList()
-# init_u = np.load(save_path + "tau.npy")[:, :-1]
-# u_init.add(init_u, interpolation=InterpolationType.EACH_FRAME)
+x_init, u_init = initial_guess.set_initial_guess_torque_driven(model, x_init, u_init, position_high, position_low, nb_shooting)
+# x_init, u_init = initial_guess.set_initial_guess_from_previous_solution_torque_driven(model, x_init, u_init, save_path, nb_shooting)
 
 # ------------- #
 
--- Marche_BiorbdOptim/squat/ocp/initial_guess_functions.py
+++ Marche_BiorbdOptim/squat/ocp/initial_guess_functions.py
@@ -16,6 +16,19 @@ class initial_guess:
         return x_init, u_init
 
     @staticmethod
+    def set_initial_guess_torque_driven(model, x_init, u_init, position_high, position_low, nb_shooting):
+        init_x = np.zeros((model.nbQ() + model.nbQdot(), nb_shooting + 1))
+        for i in range(model.nbQ()):
+            init_x[i, :] = np.concatenate((np.linspace(position_high[i], position_low[i], int(nb_shooting / 2)),
+                                           np.linspace(position_low[i], position_high[i],
+                                                       int(nb_shooting / 2) + 1))).squeeze()
+        init_x[model.nbQ():, :] = np.gradient(init_x[:model.nbQ(), :])[0]
+        x_init.add(init_x, interpolation=InterpolationType.EACH_FRAME)
+
+        u_init.add([0] * model.nbGeneralizedTorque())
+        return x_init, u_init
+
+    @staticmethod
     def set_initial_guess_from_previous_solution(model, x_init, u_init, save_path, nb_shooting):
         init_x = np.zeros((model.nbQ() + model.nbQdot(), nb_shooting + 1))
         init_x[:model.nbQ(), :] = np.load(save_path + "q.npy")
@@ -25,5 +38,16 @@ class initial_guess:
         init_u = np.zeros((model.nbGeneralizedTorque() + model.nbMuscleTotal(), nb_shooting))
         init_u[:model.nbGeneralizedTorque(), :] = np.load(save_path + "tau.npy")[:, :-1]
         init_u[model.nbGeneralizedTorque():, :] = np.load(save_path + "muscle.npy")[:, :-1]
+        u_init.add(init_u, interpolation=InterpolationType.EACH_FRAME)
+        return x_init, u_init
+
+    @staticmethod
+    def set_initial_guess_from_previous_solution_torque_driven(model, x_init, u_init, save_path, nb_shooting):
+        init_x = np.zeros((model.nbQ() + model.nbQdot(), nb_shooting + 1))
+        init_x[:model.nbQ(), :] = np.load(save_path + "q.npy")
+        init_x[model.nbQ():, :] = np.load(save_path + "qdot.npy")
+        x_init.add(init_x, interpolation=InterpolationType.EACH_FRAME)
+
+        init_u = np.load(save_path + "tau.npy")[:, :-1]
         u_init.add(init_u, interpolation=InterpolationType.EACH_FRAME)
         return x_init, u_init
