b71cf430301e59e989489dcb70d90c83e8ad93c6
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-03-30 13:23:54 -0400

    Computed muscle torque, force, jacobian

--- Marche_BiorbdOptim/squat/Compute_Results/Muscles.py
+++ Marche_BiorbdOptim/squat/Compute_Results/Muscles.py
@@ -1,6 +1,6 @@
 import numpy as np
 import biorbd
-from Utils_start import utils
+from .Utils_start import utils
 from casadi import MX, Function, vertcat
 
 
@@ -21,6 +21,8 @@ def muscular_torque(model):
                     [muscles_tau(model, qMX, dqMX, aMX)],
                     ["qMX", "dqMX", "aMX"], ["Torque"]).expand()
 
+
+
 def muscles_forces(model, q, qdot, activation):
     muscles_states = biorbd.VecBiorbdMuscleState(model.nbMuscles())
     for k in range(model.nbMuscles()):
@@ -32,13 +34,17 @@ def muscular_force(model):
     qMX = MX.sym("qMX", model.nbQ(), 1)
     dqMX = MX.sym("dqMX", model.nbQ(), 1)
     aMX = MX.sym("aMX", model.nbMuscles(), 1)
-    return Function("MuscleTorque", [qMX, dqMX, aMX],
+    return Function("MuscleForce", [qMX, dqMX, aMX],
                     [muscles_forces(model, qMX, dqMX, aMX)],
                     ["qMX", "dqMX", "aMX"], ["Forces"]).expand()
+
+
 
 def muscles_jac(model):
     qMX = MX.sym("qMX", model.nbQ(), 1)
     return Function("MuscleJac", [qMX], [model.musclesLengthJacobian(qMX).to_mx()],["qMX"], ["momentarm"]).expand()
+
+
 
 def force_isometric(model):
     fiso = []
@@ -57,6 +63,9 @@ class muscle:
         self.ocp = ocp
         self.sol = sol
         self.model = ocp.nlp[0].model
+        self.nq = self.model.nbQ()
+        self.nmus = self.model.nbMuscleTotal()
+        self.nshooting = ocp.nlp[0].ns
 
         # results data
         self.q = sol.states["q"]
@@ -66,9 +75,33 @@ class muscle:
 
         # muscles
         self.f_iso = self.set_f_iso()
+        self.muscle_force = self.set_muscle_force()
+        self.muscle_tau = self.set_muscle_tau()
+        self.muscle_jacobian = self.set_muscle_jacobian()
 
     def set_f_iso(self):
         get_forceIso = get_force_iso(self.model)
-        self.f_iso = get_forceIso()['fiso']
+        return np.array(get_forceIso()['fiso'])
+
+    def set_muscle_force(self):
+        muscle_force = np.zeros((self.nmus, self.nshooting + 1))
+        muscle_force_fcn = muscular_force(self.model)
+        for n in range(self.nshooting + 1):
+            muscle_force[:, n:n+1] = muscle_force_fcn(self.q[:, n], self.q_dot[:, n], self.activations[:, n])
+        return muscle_force
+
+    def set_muscle_tau(self):
+        muscle_tau = np.zeros((self.nq, self.nshooting + 1))
+        muscle_tau_fcn = muscular_torque(self.model)
+        for n in range(self.nshooting + 1):
+            muscle_tau[:, n:n+1] = muscle_tau_fcn(self.q[:, n], self.q_dot[:, n], self.activations[:, n])
+        return muscle_tau
+
+    def set_muscle_jacobian(self):
+        muscle_jacobian = np.zeros((self.nmus, self.nq, self.nshooting + 1))
+        muscle_jacobian_fcn = muscles_jac(self.model)
+        for n in range(self.nshooting + 1):
+            muscle_jacobian[:, :, n] = muscle_jacobian_fcn(self.q[:, n])
+        return muscle_jacobian
 
 
