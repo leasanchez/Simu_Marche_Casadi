0e5a5a7f4387ee37efb65fd46cb38740c697253f
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-03-30 13:38:38 -0400

    Set bounds for position

--- Marche_BiorbdOptim/squat/ocp/bounds_functions.py
+++ Marche_BiorbdOptim/squat/ocp/bounds_functions.py
@@ -3,7 +3,27 @@ import numpy as np
 
 class bounds:
     @staticmethod
-    def set_bounds(model, x_bounds, u_bounds, mapping=False):
+    def set_bounds(model, x_bounds, u_bounds, position_high, mapping=False):
+        torque_min, torque_max = -1000, 1000
+        activation_min, activation_max = 1e-3, 0.99
+
+        x_bounds.add(bounds=QAndQDotBounds(model))
+        x_bounds[0][:len(position_high), 0] = np.array(position_high)
+        x_bounds[0][:len(position_high), -1] = np.array(position_high)
+        if mapping:
+            u_bounds.add(
+                [torque_min] * (model.nbGeneralizedTorque() - model.nbRoot()) + [activation_min] * model.nbMuscleTotal(),
+                [torque_max] * (model.nbGeneralizedTorque() - model.nbRoot()) + [activation_max] * model.nbMuscleTotal(),
+            )
+        else:
+            u_bounds.add(
+                [torque_min] * model.nbGeneralizedTorque() + [activation_min] * model.nbMuscleTotal(),
+                [torque_max] * model.nbGeneralizedTorque() + [activation_max] * model.nbMuscleTotal(),
+            )
+        return x_bounds, u_bounds
+
+    @staticmethod
+    def set_bounds_position(model, x_bounds, u_bounds, mapping=False):
         torque_min, torque_max = -1000, 1000
         activation_min, activation_max = 1e-3, 0.99
 
