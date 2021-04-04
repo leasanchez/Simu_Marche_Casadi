2c3940e893a4009bb3f17f113cc8f6b1bdeb59c3
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-04-04 10:27:20 -0400

    Bounds torque driven

--- Marche_BiorbdOptim/squat/ocp/bounds_functions.py
+++ Marche_BiorbdOptim/squat/ocp/bounds_functions.py
@@ -3,27 +3,7 @@ import numpy as np
 
 class bounds:
     @staticmethod
-    def set_bounds(model, x_bounds, u_bounds, position_high, mapping=False):
-        torque_min, torque_max = -1000, 1000
-        activation_min, activation_max = 1e-3, 0.99
-
-        x_bounds.add(bounds=QAndQDotBounds(model))
-        x_bounds[0][:len(position_high), 0] = np.array(position_high)
-        x_bounds[0][:len(position_high), -1] = np.array(position_high)
-        if mapping:
-            u_bounds.add(
-                [torque_min] * (model.nbGeneralizedTorque() - model.nbRoot()) + [activation_min] * model.nbMuscleTotal(),
-                [torque_max] * (model.nbGeneralizedTorque() - model.nbRoot()) + [activation_max] * model.nbMuscleTotal(),
-            )
-        else:
-            u_bounds.add(
-                [torque_min] * model.nbGeneralizedTorque() + [activation_min] * model.nbMuscleTotal(),
-                [torque_max] * model.nbGeneralizedTorque() + [activation_max] * model.nbMuscleTotal(),
-            )
-        return x_bounds, u_bounds
-
-    @staticmethod
-    def set_bounds_position(model, x_bounds, u_bounds, mapping=False):
+    def set_bounds(model, x_bounds, u_bounds, mapping=False):
         torque_min, torque_max = -1000, 1000
         activation_min, activation_max = 1e-3, 0.99
 
@@ -41,15 +21,11 @@ class bounds:
         return x_bounds, u_bounds
 
     @staticmethod
-    def set_bounds_torque_driven(model, x_bounds, u_bounds, position_init):
+    def set_bounds_torque_driven(model, position_init, x_bounds, u_bounds):
         torque_min, torque_max = -1000, 1000
 
         x_bounds.add(bounds=QAndQDotBounds(model))
-        x_bounds[0].min[:model.nbQ(), 0] = np.array(position_init).squeeze()
-        x_bounds[0].max[:model.nbQ(), 0] = np.array(position_init).squeeze()
-
-        x_bounds[0].min[:model.nbQ(), -1] = np.array(position_init).squeeze()
-        x_bounds[0].max[:model.nbQ(), -1] = np.array(position_init).squeeze()
+        # x_bounds[0][model.nbQ():, (0, -1)] = 0
 
         u_bounds.add(
             [torque_min] * model.nbGeneralizedTorque(),
