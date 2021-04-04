c4f696880a7db952b75a9b25ddeb4c92abe74deb
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-04-04 10:26:43 -0400

    Optimization position

--- Marche_BiorbdOptim/squat/Squat_position_basse_torque_driven.py
+++ Marche_BiorbdOptim/squat/Squat_position_basse_torque_driven.py
@@ -25,16 +25,14 @@ def get_results(sol):
     q = sol.states["q"]
     qdot = sol.states["qdot"]
     tau = sol.controls["tau"]
-    muscle = sol.controls["muscles"]
-    return q, qdot, tau, muscle
+    return q, qdot, tau
 
 def save_results(ocp, sol, save_path):
     ocp.save(sol, save_path + 'cycle.bo')
-    q, qdot, tau, muscle = get_results(sol.merge_phases())
+    q, qdot, tau = get_results(sol.merge_phases())
     np.save(save_path + 'qdot', qdot)
     np.save(save_path + 'q', q)
     np.save(save_path + 'tau', tau)
-    np.save(save_path + 'muscle', muscle)
 
 
 model = biorbd.Model("models/2legs_18dof_flatfootR.bioMod")
@@ -45,30 +43,30 @@ nb_qdot = model.nbQdot()
 nb_tau = model.nbGeneralizedTorque()
 nb_mus = model.nbMuscleTotal()
 nb_mark = model.nbMarkers()
-nb_shooting = 15
+nb_shooting = 30
 final_time = 0.5
 
 # --- Subject positions and initial trajectories --- #
-position_high = [[0], [-0.07], [0], [0], [0], [-0.4],
+position_high = [[0], [-0.054], [0], [0], [0], [-0.4],
                 [0], [0], [0.37], [-0.13], [0], [0.11],
                 [0], [0], [0.37], [-0.13], [0], [0.11]]
 position_low = [-0.06, -0.36, 0, 0, 0, -0.8,
                 0, 0, 1.53, -1.55, 0, 0.68,
                 0, 0, 1.53, -1.55, 0, 0.68]
 
+
 # --- Compute CoM position --- #
 compute_CoM = biorbd.to_casadi_func("CoM", model.CoM, MX.sym("q", nb_q, 1))
 CoM_high = compute_CoM(np.array(position_high))
-q_high = np.load('./RES/muscle_driven/position_init_final/q.npy')[:, -1]
 
 # --- Define Optimal Control Problem --- #
 # Objective function
 objective_functions = ObjectiveList()
-objective_functions = objective.set_objectif_function_position_basse(objective_functions, position_high)
+# objective_functions = objective.set_objectif_function_position_basse_torque_driven(objective_functions, position_high)
 
 # Dynamics
 dynamics = DynamicsList()
-dynamics.add(DynamicsFcn.MUSCLE_ACTIVATIONS_AND_TORQUE_DRIVEN_WITH_CONTACT)
+dynamics.add(DynamicsFcn.TORQUE_DRIVEN_WITH_CONTACT)
 
 # Constraints
 constraints = ConstraintList()
@@ -77,12 +75,12 @@ constraints = constraint.set_constraints_position_basse(constraints, inequality_
 # Path constraints
 x_bounds = BoundsList()
 u_bounds = BoundsList()
-x_bounds, u_bounds = bounds.set_bounds(model, x_bounds, u_bounds, mapping=False)
+x_bounds, u_bounds = bounds.set_bounds_torque_driven(model, position_high, x_bounds, u_bounds)
 
 # Initial guess
 x_init = InitialGuessList()
 u_init = InitialGuessList()
-x_init, u_init = initial_guess.set_initial_guess_position_basse(model, x_init, u_init, position_high, position_low, nb_shooting, mapping=False)
+x_init, u_init = initial_guess.set_initial_guess_position_basse_torque_driven(model, x_init, u_init, position_high, position_low, nb_shooting, mapping=False)
 
 # ------------- #
 ocp = OptimalControlProgram(
@@ -96,12 +94,8 @@ ocp = OptimalControlProgram(
     u_bounds=u_bounds,
     objective_functions=objective_functions,
     constraints=constraints,
-    n_threads=6,
+    n_threads=8,
 )
-
-# ocp_prev, sol_prev = ocp.load('./RES/muscle_driven/position_init_final/cycle.bo')
-# q_prev = sol_prev.states["q"]
-# sol_prev.animate()
 
 # --- Solve the program --- #
 tic = time()
@@ -119,7 +113,7 @@ sol = ocp.solve(
 toc = time() - tic
 
 # --- Save results --- #
-save_path = './RES/muscle_driven/position_basse/'
+save_path = './RES/torque_driven/position_basse/'
 save_results(ocp, sol, save_path)
 
 # --- Plot CoP --- #
@@ -131,20 +125,23 @@ plt.plot(cop[2, :])
 
 # --- Plot markers --- #
 markers = biorbd.to_casadi_func("markers", model.markers, MX.sym("q", nb_q, 1))
+markers_velocity = biorbd.to_casadi_func("markers_velocity", model.markersVelocity, MX.sym("q", nb_q, 1), MX.sym("qdot", nb_q, 1))
 contact_idx_right = (31, 32, 33)
 contact_idx_left = (55, 56, 57)
 q = sol.states["q"]
+qdot = sol.states["qdot"]
 markers_pos = np.zeros((3, nb_mark, q.shape[1]))
+markers_vel = np.zeros((3, nb_mark, q.shape[1]))
 for n in range(q.shape[1]):
     markers_pos[:, :, n] = markers(q[:, n:n+1])
+    markers_vel[:, :, n] = markers_velocity(q[:, n:n + 1], qdot[:, n:n+1])
 
 # --- Plot Symetry --- #
-plot_result = Affichage(ocp, sol, muscles=True)
+plot_result = Affichage(ocp, sol, muscles=False)
 plot_result.plot_q_symetry()
 plot_result.plot_tau_symetry()
 plot_result.plot_qdot_symetry()
 plot_result.plot_individual_forces()
-plot_result.plot_muscles_symetry()
 
 # --- Show results --- #
 sol.animate()
