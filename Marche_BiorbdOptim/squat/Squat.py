ab228e0c506bd21803eb620f041896087986abb2
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-04-23 08:41:10 -0400

    Update squat 2D

--- Marche_BiorbdOptim/squat/Squat.py
+++ Marche_BiorbdOptim/squat/Squat.py
@@ -1,34 +1,40 @@
+import biorbd
+import bioviz
 import numpy as np
-from casadi import dot, Function, vertcat, MX, mtimes, nlpsol, mmax
+from casadi import MX, Function
 import biorbd
 import bioviz
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
+from time import time
 from bioptim import (
     OptimalControlProgram,
     DynamicsList,
     DynamicsFcn,
     BoundsList,
-    Bounds,
-    QAndQDotBounds,
     InitialGuessList,
-    InitialGuess,
     ObjectiveList,
     ObjectiveFcn,
-    InterpolationType,
-    Node,
     ConstraintList,
     ConstraintFcn,
     Solver,
+    Node,
     PenaltyNodes,
+    InterpolationType,
+    QAndQDotBounds,
 )
 
 from ocp.objective_functions import objective
 from ocp.constraint_functions import constraint
 from ocp.bounds_functions import bounds
 from ocp.initial_guess_functions import initial_guess
+from Compute_Results.Plot_results import Affichage
+from Compute_Results.Contact_Forces import contact
 
+def custom_CoM_position(pn: PenaltyNodes, value: float) -> MX:
+    nq = pn.nlp.shape["q"]
+    compute_CoM = biorbd.to_casadi_func("CoM", pn.nlp.model.CoM, pn.nlp.q)
+    com = compute_CoM(pn.x[0][:nq])
+    return com[2] - value
 
 def get_results(sol):
     q = sol.states["q"]
@@ -45,68 +51,105 @@ def save_results(ocp, sol, save_path):
 
 
 # OPTIMAL CONTROL PROBLEM
-model = biorbd.Model("Modeles_S2M/2legs_18dof_flatfootR.bioMod")
+model = (biorbd.Model("models/2legs_18dof_flatfootR_2D.bioMod"),
+         biorbd.Model("models/2legs_18dof_flatfootR_2D.bioMod"), )
 
 # --- Problem parameters --- #
-nb_q = model.nbQ()
-nb_qdot = model.nbQdot()
-nb_tau = model.nbGeneralizedTorque()
-nb_mus = model.nbMuscleTotal()
-nb_shooting = 30
-final_time=1.0
+nb_q = model[0].nbQ()
+nb_qdot = model[0].nbQdot()
+nb_tau = model[0].nbGeneralizedTorque()
+nb_mus = model[0].nbMuscleTotal()
+nb_shooting = (20, 20)
+final_time = (0.5, 0.5)
+time_min = (0.2, 0.2)
+time_max = (1.0, 1.0)
+
+c = model[0].contactNames()
+for (i, name) in enumerate(c):
+    print(f"{i} : {name.to_string()}")
+
 
 # --- Subject positions and initial trajectories --- #
 position_zeros = [0]*nb_q
-position_high = [[0], [-0.07], [0], [0], [0], [-0.4],
-                [0], [0], [0.37], [-0.13], [0], [0.11],
-                [0], [0], [0.37], [-0.13], [0], [0.11]]
-position_low = [-0.06, -0.36, 0, 0, 0, -0.8,
-                0, 0, 1.53, -1.55, 0, 0.68,
-                0, 0, 1.53, -1.55, 0, 0.68]
+position_high = [[0], [-0.07], [-0.4],
+                [0.37], [-0.13], [0.11],
+                [0.37], [-0.13], [0.11]]
+position_low = [-0.06, -0.36, -0.8,
+                1.53, -1.55, 0.68,
+                1.53, -1.55, 0.68]
+
+q_init_fall = np.zeros((nb_q, nb_shooting[0] + 1))
+q_init_climb = np.zeros((nb_q, nb_shooting[1] + 1))
+for i in range(nb_q):
+    q_init_fall[i, :] = np.linspace(position_high[i], position_low[i], nb_shooting[0]+1).squeeze()
+    q_init_climb[i, :] = np.linspace(position_low[i], position_high[i], nb_shooting[1]+1).squeeze()
 
 # --- Compute CoM position --- #
-symbolic_q = MX.sym("q", nb_q, 1)
-compute_CoM = Function(
-    "ComputeCoM",
-    [symbolic_q],
-    [model.CoM(symbolic_q).to_mx()],
-    ["q"],
-    ["CoM"],
-).expand()
-
+compute_CoM = biorbd.to_casadi_func("CoM", model[0].CoM, MX.sym("q", nb_q, 1))
 CoM_high = compute_CoM(np.array(position_high))
 CoM_low = compute_CoM(np.array(position_low))
-
-# # --- Animate model --- #
-# b = bioviz.Viz(loaded_model=model, show_muscles=False, show_segments_center_of_mass=False, show_local_ref_frame=False)
-# b.set_q(np.array(position_high).squeeze())
-# # b.set_q(np.array(position_high))
-# b.load_movement(q_init)
-# b.exec()
-
-
-# --- Objective function --- #
-objective_functions = ObjectiveList()
-objective_functions = objective.set_objectif_function_torque_driven(objective_functions)
 
 # --- Dynamics --- #
 dynamics = DynamicsList()
 dynamics.add(DynamicsFcn.TORQUE_DRIVEN_WITH_CONTACT)
+dynamics.add(DynamicsFcn.TORQUE_DRIVEN_WITH_CONTACT)
+
+# --- Objective function --- #
+objective_functions = ObjectiveList()
+objective_functions.add(custom_CoM_position,
+                        custom_type=ObjectiveFcn.Mayer,
+                        value=-0.25,
+                        node=Node.END,
+                        quadratic=True,
+                        weight=100,
+                        phase=0)
+
+objective_functions.add(ObjectiveFcn.Mayer.TRACK_STATE,
+                        node=Node.END,
+                        quadratic=True,
+                        target=np.concatenate([np.array(position_high).squeeze(), np.zeros(nb_qdot)]).reshape(nb_q + nb_qdot, 1),
+                        weight=10,
+                        phase=1)
 
 # --- Constraints --- #
 constraints = ConstraintList()
-constraints = constraint.set_constraints(constraints)
+n_phases = 2
+for i in range(n_phases):
+    # Contact forces
+    contact_z_axes = (1, 2, 4, 5)
+    for c in contact_z_axes:
+        constraints.add(  # positive vertical forces
+            ConstraintFcn.CONTACT_FORCE,
+            min_bound=0,
+            max_bound=np.inf,
+            node=Node.ALL,
+            contact_force_idx=c,
+            phase=i,
+        )
 
 # --- Path constraints --- #
 x_bounds = BoundsList()
 u_bounds = BoundsList()
-x_bounds, u_bounds = bounds.set_bounds_torque_driven(model, x_bounds, u_bounds, position_high)
+x_bounds.add(bounds=QAndQDotBounds(model[0]))
+x_bounds.add(bounds=QAndQDotBounds(model[0]))
+x_bounds[0][:, 0] = np.concatenate([np.array(position_high).squeeze(), np.zeros(nb_qdot)])
+# x_bounds[1][:, -1] = np.concatenate([np.array(position_high).squeeze(), np.zeros(nb_qdot)])
+
+u_bounds = BoundsList()
+u_bounds.add([0] * nb_tau,[1000] * nb_tau)
+u_bounds.add([0] * nb_tau,[1000] * nb_tau)
 
 # --- Initial guess --- #
 x_init = InitialGuessList()
+x = np.zeros((model[0].nbQ() + model[0].nbQ(), nb_shooting[0]+1))
+x[:nb_q, :] =q_init_fall
+x_init.add(x, interpolation=InterpolationType.EACH_FRAME)
+x[:nb_q, :] =q_init_climb
+x_init.add(x, interpolation=InterpolationType.EACH_FRAME)
+
 u_init = InitialGuessList()
-x_init, u_init = initial_guess.set_initial_guess_torque_driven(model, x_init, u_init, position_high, position_low, nb_shooting)
-# x_init, u_init = initial_guess.set_initial_guess_from_previous_solution_torque_driven(model, x_init, u_init, save_path, nb_shooting)
+u_init.add([0] * model[0].nbGeneralizedTorque())
+u_init.add([0] * model[0].nbGeneralizedTorque())
 
 # ------------- #
 
