d48332f2aeb856745cef15b1cff47a9cb2064129
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-06-11 13:27:25 -0400

    Used anatomical position as threshold

--- Marche_BiorbdOptim/squat/compute_exp_data/MARKERS.py
+++ Marche_BiorbdOptim/squat/compute_exp_data/MARKERS.py
@@ -7,19 +7,17 @@ from ezc3d import c3d
 def get_exp_files(path):
     return os.listdir(path + '/Squats/')
 
-def find_initial_height(marker_position):
-    return int(np.mean(marker_position[:101]))
+def find_initial_height(marker_anato):
+    return round(np.mean(marker_anato))
 
-def find_indices(markers_position):
-    pelvis_center = np.mean(markers_position[2, :6, :], axis=0)
-    index_CENTER = np.where(pelvis_center > (find_initial_height(pelvis_center) - 5))[0]
-    discontinuities_idx = np.where(np.gradient(index_CENTER) > 1)[0]
-    return index_CENTER[discontinuities_idx]
+def find_indices(markers_position, marker_anato):
+    index_anato = np.where(markers_position[2, 0, :] > (find_initial_height(marker_anato) - 5))[0]
+    discontinuities_idx = np.where(np.gradient(index_anato) > 1)[0]
+    return index_anato[discontinuities_idx]
 
 class markers:
     def __init__(self, path):
         self.path = path
-        # self.list_exp_files = get_exp_files(path)
         self.list_exp_files = ['squat_controle.c3d', 'squat_3cm.c3d', 'squat_4cm.c3d', 'squat_5cm.c3d', 'squat_controle_post.c3d']
         self.loaded_c3d = self.load_c3d()
         self.labels_markers = self.loaded_c3d[-1]["parameters"]["POINT"]["LABELS"]["value"][:52]
@@ -43,19 +41,21 @@ class markers:
     def get_events(self):
         events=[]
         markers_position = self.get_markers_position()
+        position_anato = c3d(self.path + '/SCoRE/anato.c3d')
+        marker_anato = position_anato["data"]["points"]
 
         for mark in markers_position:
-            indices = find_indices(mark)
+            indices = find_indices(mark, marker_anato[2, 0, :])
 
-            pelvis_center = np.mean(mark[2, :6, :], axis=0)
-            init_CENTER = find_initial_height(pelvis_center)
+            init_anato = find_initial_height(marker_anato[2, 0, :]) - 5
             events.append(indices)
 
-            # plt.figure()
-            # plt.plot(pelvis_center)
-            # plt.plot([0, 1900], [init_CENTER - 5, init_CENTER - 5], 'k--')
-            # for idx in indices:
-            #     plt.plot([idx, idx], [700, 1050], 'g--')
-            # plt.show()
+            plt.figure()
+            plt.plot(mark[2, 0, :])
+            plt.plot(marker_anato[2, 0, :], 'r')
+            plt.plot([0, 1900], [init_anato, init_anato], 'k--')
+            for idx in indices:
+                plt.plot([idx, idx], [700, 1050], 'g--')
+            plt.show()
 
         return events
