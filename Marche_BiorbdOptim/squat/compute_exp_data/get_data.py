d260fb2e5a6ee3436c9c4cc68d9c7700ff33dce5
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-06-18 16:11:02 -0400

    Add force plateform

--- Marche_BiorbdOptim/squat/compute_exp_data/get_data.py
+++ Marche_BiorbdOptim/squat/compute_exp_data/get_data.py
@@ -7,14 +7,18 @@ from FORCE_PLATFORM import force_platform
 name = 'AmeCeg'
 
 # emg
-emg_test = emg(name)
+# emg_test = emg(name)
+#
+# emg_test.plot_squat_repetition()
+# emg_test.plot_squat_mean()
+# emg_test.plot_squat_comparison()
+# emg_test.plot_squat_comparison_5cm()
+# plt.show()
 
-emg_test.plot_squat_repetition()
-emg_test.plot_squat_mean()
-emg_test.plot_squat_comparison()
-emg_test.plot_squat_comparison_5cm()
+
+# force plateforme
+contact_test = force_platform(name)
+contact_test.plot_force_repetition()
+contact_test.plot_force_mean()
+contact_test.plot_force_comparison()
 plt.show()
-
-
-# contact
-# force_platform_test = force_platform(name)
