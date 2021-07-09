55c021d74d5773ca1239ea1d12a807a601ebedc1
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-07-09 18:01:45 -0400

    Add kinematic

--- Marche_BiorbdOptim/squat/compute_exp_data/get_data.py
+++ Marche_BiorbdOptim/squat/compute_exp_data/get_data.py
@@ -4,18 +4,25 @@ from EMG import emg
 import biorbd
 import bioviz
 from FORCE_PLATFORM import force_platform
+from KINEMATIC import kinematic
 
 
 name = 'AmeCeg'
-model_path = '/home/leasanchez/programmation/Simu_Marche_Casadi/Marche_BiorbdOptim/squat/Data_test/AmeCeg/AmeCeg.bioMod'
+model_path = "/home/leasanchez/programmation/Simu_Marche_Casadi/Marche_BiorbdOptim/squat/Data_test/" + name + "/" + name + ".bioMod"
 model = biorbd.Model(model_path)
 # b = bioviz.Viz(loaded_model=model)
+
+# kinematic
+kin_test = kinematic(name)
+kin_test.plot_squat_mean()
+kin_test.plot_squat_repetition()
 
 # emg
 emg_test = emg(name)
 emg_test.plot_squat_mean_symetry()
+emg_test.plot_assymetry_comparison_5cm()
 emg_test.plot_squat_repetition()
-emg_test.plot_squat_mean()
+emg_test.plot_squat_mean('squat_controle.c3d')
 emg_test.plot_squat_comparison()
 emg_test.plot_squat_comparison_5cm()
 plt.show()
