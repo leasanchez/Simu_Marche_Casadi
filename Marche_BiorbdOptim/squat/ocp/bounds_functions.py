5dd842ec87400ecacf866ecb4bad38a525f00648
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-07-21 11:14:46 -0400

    Update bounds + remove unnecessary functions

--- Marche_BiorbdOptim/squat/ocp/bounds_functions.py
+++ Marche_BiorbdOptim/squat/ocp/bounds_functions.py
@@ -1,39 +1,27 @@
 from bioptim import QAndQDotBounds, BiMapping
-import numpy as np
 
 class bounds:
     @staticmethod
-    def set_bounds(model, x_bounds, u_bounds, mapping=False):
+    def set_bounds(model, x_bounds, u_bounds, muscles=False, mapping=False):
         torque_min, torque_max = -1000, 1000
         activation_min, activation_max = 1e-3, 0.99
+        if muscles:
+            nb_mus = model.nbMuscleTotal()
+        else:
+            nb_mus = 0
 
         x_bounds.add(bounds=QAndQDotBounds(model))
         if mapping:
             u_bounds.add(
-                [torque_min] * (model.nbGeneralizedTorque() - model.nbRoot()) + [activation_min] * model.nbMuscleTotal(),
-                [torque_max] * (model.nbGeneralizedTorque() - model.nbRoot()) + [activation_max] * model.nbMuscleTotal(),
+                [torque_min] * (model.nbGeneralizedTorque() - model.nbRoot()) + [activation_min] * nb_mus,
+                [torque_max] * (model.nbGeneralizedTorque() - model.nbRoot()) + [activation_max] * nb_mus,
             )
         else:
             u_bounds.add(
-                [torque_min] * model.nbGeneralizedTorque() + [activation_min] * model.nbMuscleTotal(),
-                [torque_max] * model.nbGeneralizedTorque() + [activation_max] * model.nbMuscleTotal(),
+                [torque_min] * model.nbGeneralizedTorque() + [activation_min] * nb_mus,
+                [torque_max] * model.nbGeneralizedTorque() + [activation_max] * nb_mus,
             )
-        return x_bounds, u_bounds
 
-    @staticmethod
-    def set_bounds_torque_driven(model, position_init, x_bounds, u_bounds):
-        torque_min, torque_max = -1000, 1000
-        n_phases = 2
-        for i in range(n_phases):
-            x_bounds.add(bounds=QAndQDotBounds(model))
-
-            u_bounds.add(
-                [torque_min] * model.nbGeneralizedTorque(),
-                [torque_max] * model.nbGeneralizedTorque(),
-            )
-        x_bounds[0][:model.nbQ(), 0] = position_init
-        x_bounds[1][:model.nbQ(), -1] = position_init
-        return x_bounds, u_bounds
 
     @staticmethod
     def set_mapping():
