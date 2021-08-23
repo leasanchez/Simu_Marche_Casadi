f4c31d7022be16df310cf69fc346299894a5f265
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-08-23 09:15:46 -0400

    Update multiphase

--- Marche_BiorbdOptim/squat/Squat_multiphase.py
+++ Marche_BiorbdOptim/squat/Squat_multiphase.py
@@ -1,5 +1,5 @@
 import biorbd_casadi as biorbd
-import bioviz
+# import bioviz
 import numpy as np
 from casadi import MX, Function
 from matplotlib import pyplot as plt
@@ -13,6 +13,7 @@ from bioptim import (
     ConstraintList,
     InitialGuessList,
     Solver,
+    OdeSolver,
 )
 
 from ocp.load_data import data
@@ -42,28 +43,32 @@ def save_results(ocp, sol, save_path):
 
 # --- Problem parameters --- #
 nb_shooting = (19, 19)
-final_time = (0.5, 0.5)
+final_time = (0.75, 0.75)
 
-# # experimental data
+# experimental data
 # name = "AmeCeg"
 # model_path = "/home/leasanchez/programmation/Simu_Marche_Casadi/Marche_BiorbdOptim/squat/Data_test/" + name + "/" + name + ".bioMod"
 # model = (biorbd.Model(model_path),
 #          biorbd.Model(model_path),)
 # # --- Subject positions and initial trajectories --- #
+# q_ref = []
 # q_kalman = data.get_q(name='AmeCeg', title='squat_controle')
 # position_high = q_kalman[:, 0]
 # position_low = q_kalman[:, 100]
+# a = q_kalman[:, 0::5]
+# q_ref.append(a[:, :nb_shooting[0]+1])
+# q_ref.append(a[:, nb_shooting[0]+1:])
 
 # model init
 model = (biorbd.Model("models/2legs_18dof_flatfootR.bioMod"),
          biorbd.Model("models/2legs_18dof_flatfootR.bioMod"))
 # --- Subject positions and initial trajectories --- #
-position_high = [0, -0.054, 0, 0, 0, -0.4,
-                 0, 0, 0.37, -0.13, 0, 0.11,
-                 0, 0, 0.37, -0.13, 0, 0.11]
-position_low = [-0.12, -0.43, 0.0, 0.0, 0.0, -0.74,
-                0.0, 0.0, 1.82, -1.48, 0.0, 0.36,
-                0.0, 0.0, 1.82, -1.48, 0.0, 0.36]
+position_high = [0.0, 0.246, 0.0, 0.0, 0.0, -0.4,
+                 0.0, 0.0, 0.37, -0.13, 0.0, 0.0, 0.11,
+                 0.0, 0.0, 0.37, -0.13, 0.0, 0.0, 0.11]
+position_low = [-0.12, -0.13, 0.0, 0.0, 0.0, -0.74,
+                0.0, 0.0, 1.82, -1.48, 0.0, 0.0, 0.36,
+                0.0, 0.0, 1.82, -1.48, 0.0, 0.0, 0.36]
 q_ref = []
 q_ref.append(np.linspace(position_high, position_low, nb_shooting[0] + 1).T)
 q_ref.append(np.linspace(position_low, position_high, nb_shooting[0] + 1).T)
@@ -76,27 +81,30 @@ CoM_low = compute_CoM(np.array(position_low))
 # --- Define Optimal Control Problem --- #
 # Dynamics
 dynamics = DynamicsList()
-dynamics.add(DynamicsFcn.MUSCLE_DRIVEN, with_residual_torque=True, with_contact=False, phase=0)
-dynamics.add(DynamicsFcn.MUSCLE_DRIVEN, with_residual_torque=True, with_contact=False, phase=1)
+dynamics.add(DynamicsFcn.TORQUE_DRIVEN, with_contact=True, phase=0, expand=False)
+dynamics.add(DynamicsFcn.TORQUE_DRIVEN, with_contact=True, phase=1, expand=False)
+# dynamics.add(DynamicsFcn.MUSCLE_DRIVEN, with_residual_torque=True, with_contact=True, phase=0, expand=False)
+# dynamics.add(DynamicsFcn.MUSCLE_DRIVEN, with_residual_torque=True, with_contact=True, phase=1, expand=False)
 
 # Objective function
 objective_functions = ObjectiveList()
-objective.set_objectif_function_multiphase(objective_functions, muscles=False)
+objective.set_objectif_function_multiphase(objective_functions, position_high, muscles=False)
 
 # Constraints
 constraints = ConstraintList()
-# constraints = constraint.set_constraints_multiphase(constraints, inequality_value=0.0)
+constraint.set_constraints_multiphase(constraints, inequality_value=0.0)
 
 # Path constraints
 x_bounds = BoundsList()
 u_bounds = BoundsList()
-x_bounds, u_bounds = bounds.set_bounds(model[0], x_bounds, u_bounds, mapping=False)
-x_bounds, u_bounds = bounds.set_bounds(model[1], x_bounds, u_bounds, mapping=False)
+bounds.set_bounds(model[0], x_bounds, u_bounds, muscles=False, mapping=False)
+bounds.set_bounds(model[1], x_bounds, u_bounds, muscles=False, mapping=False)
+x_bounds[0][:model[0].nbQ(), 0] = position_high
 
 # Initial guess
 x_init = InitialGuessList()
 u_init = InitialGuessList()
-x_init, u_init = initial_guess.set_initial_guess_multiphase(model, x_init, u_init, q_ref, muscles=True, mapping=False)
+initial_guess.set_initial_guess_multiphase(model, x_init, u_init, q_ref, muscles=False, mapping=False)
 
 # ------------- #
 ocp = OptimalControlProgram(
@@ -111,6 +119,7 @@ ocp = OptimalControlProgram(
     objective_functions=objective_functions,
     constraints=constraints,
     n_threads=8,
+    # ode_solver=OdeSolver.COLLOCATION(method='radau', polynomial_degree=4),
 )
 
 # --- Solve the program --- #
@@ -119,8 +128,8 @@ sol = ocp.solve(
     solver=Solver.IPOPT,
     solver_options={
         "ipopt.tol": 1e-6,
-        "ipopt.max_iter": 5000,
-        "ipopt.hessian_approximation": "exact",
+        "ipopt.max_iter": 2000,
+        "ipopt.hessian_approximation": "limited-memory",
         "ipopt.limited_memory_max_history": 50,
         "ipopt.linear_solver": "ma57",
     },
