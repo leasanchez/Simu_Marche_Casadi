812c7d20454af699931e705458808401064a0787
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-09-14 12:19:13 -0400

    compute time from events

--- Marche_BiorbdOptim/squat/compute_exp_data/MARKERS.py
+++ Marche_BiorbdOptim/squat/compute_exp_data/MARKERS.py
@@ -51,6 +51,7 @@ class markers:
         self.path = path
         self.list_exp_files = ['squat_controle.c3d', 'squat_3cm.c3d', 'squat_4cm.c3d', 'squat_5cm.c3d', 'squat_controle_post.c3d']
         self.loaded_c3d = self.load_c3d()
+        self.n_marker = 52
         self.labels_markers = self.loaded_c3d[-1]["parameters"]["POINT"]["LABELS"]["value"][:52]
         self.markers_position = self.get_markers_position()
         self.events, self.mid_events = self.get_events()
@@ -86,40 +87,8 @@ class markers:
     def get_time(self):
         time = []
         for (n,e) in enumerate(self.events):
-            t = 0
-            t_fall = 0
-            t_climb = 0
-            for i in range(int(len(e)/2)):
-                t += (e[2*i + 1] - e[2*i])/100
-                t_fall += (self.mid_events[n][i] - e[2*i])/100
-                t_climb += (e[2 * i + 1] - self.mid_events[n][i]) / 100
-            time.append([t_fall/6, t_climb/6, t/6])
+            t = np.mean((np.array(e[1::2]) - np.array(e[0::2]))/100)
+            tfall = np.mean((self.mid_events[n] - np.array(e[0::2]))/100)
+            tclimb = np.mean((np.array(e[1::2]) - self.mid_events[n])/100)
+            time.append([t, tfall, tclimb])
         return time
-
-    def get_mid_events(self):
-        mid_events=[]
-        for event in self.events:
-            md = []
-            for i in range(int(len(event)/2)):
-                md.append(int(np.mean([event[2*i], event[2*i + 1]])))
-            mid_events.append(np.array(md))
-
-        markers_position = self.get_markers_position()
-        position_anato = c3d(self.path + '/SCoRE/anato.c3d')
-        marker_anato = position_anato["data"]["points"]
-
-        for (i, mark) in enumerate(markers_position):
-            indices = find_indices(mark[2, 0, :], marker_anato[2, 0, :])
-
-            init_anato = find_initial_height(marker_anato[2, 0, :])
-
-            # plt.figure()
-            # plt.plot(mark[2, 0, :])
-            # plt.plot(marker_anato[2, 0, :], 'r')
-            # plt.plot([0, 1900], [init_anato, init_anato], 'k--')
-            # for idx in indices:
-            #     plt.plot([idx, idx], [700, 1050], 'g--')
-            # for m in range(mid_events[i].shape[0]):
-            #     plt.plot([mid_events[i][m], mid_events[i][m]], [612, 1050], 'm--')
-            # plt.show()
-        return mid_events
