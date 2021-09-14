381697f3b6085cd02f9868d25bd5558dd20e0892
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-09-14 12:18:54 -0400

    find events/mid_events (+plot)

--- Marche_BiorbdOptim/squat/compute_exp_data/MARKERS.py
+++ Marche_BiorbdOptim/squat/compute_exp_data/MARKERS.py
@@ -8,18 +8,43 @@ def get_exp_files(path):
     return os.listdir(path + '/Squats/')
 
 def find_initial_height(marker_anato):
-    return round(np.mean(marker_anato))
+    m = marker_anato[~np.isnan(marker_anato)]
+    return round(np.mean(m))
 
 def find_indices(markers_position, marker_anato):
     indices = []
+    # index_anato = np.where(markers_position > (find_initial_height(markers_position[:50]) - 10))[0]
     index_anato = np.where(markers_position > (find_initial_height(marker_anato) - 5))[0]
     discontinuities_idx = np.where(np.gradient(index_anato) > 1)[0]
     for idx in index_anato[discontinuities_idx]:
         a = np.where((markers_position[idx:idx + 51] - markers_position[idx]) < 0)[0]
         b = np.where((markers_position[idx - 50:idx + 1] - markers_position[idx]) < 0)[0]
-        if (a.size > 45) or (b.size > 45):
+        if (a.size > 40) or (b.size > 40):
             indices.append(idx)
+
+    plt.figure()
+    plt.plot(markers_position)
+    plt.plot([0, 1900], [find_initial_height(marker_anato), find_initial_height(marker_anato)], 'k--')
+    plt.plot([0, 1900], [find_initial_height(marker_anato) - 5, find_initial_height(marker_anato) - 5], 'g--')
+    for idx in indices:
+        plt.plot([idx, idx], [700, 1050], 'r')
+    plt.show()
     return indices
+
+def find_min(markers_position, events):
+    min=[]
+    n_repet = int(len(events)/2)
+    for i in range(n_repet):
+        min.append(np.argmin(markers_position[events[2*i]:events[2*i + 1]]) + events[2*i])
+
+    # plt.figure()
+    # plt.plot(markers_position)
+    # for e in events:
+    #     plt.plot([e, e], [700, 1050], 'r')
+    # for m in min:
+    #     plt.plot([m, m], [700, 1050], 'g')
+    # plt.show()
+    return min
 
 class markers:
     def __init__(self, path):
@@ -28,8 +53,7 @@ class markers:
         self.loaded_c3d = self.load_c3d()
         self.labels_markers = self.loaded_c3d[-1]["parameters"]["POINT"]["LABELS"]["value"][:52]
         self.markers_position = self.get_markers_position()
-        self.events = self.get_events()
-        self.mid_events = self.get_mid_events()
+        self.events, self.mid_events = self.get_events()
         self.time = self.get_time()
 
 
@@ -42,31 +66,22 @@ class markers:
     def get_markers_position(self):
         markers_position = []
         for c in self.loaded_c3d:
-            markers_position.append(c["data"]["points"])
+            markers_position.append(c["data"]["points"][:3, :, :])
         return markers_position
-
 
     def get_events(self):
         events=[]
+        mid_events=[]
         markers_position = self.get_markers_position()
         position_anato = c3d(self.path + '/SCoRE/anato.c3d')
         marker_anato = position_anato["data"]["points"]
 
         for mark in markers_position:
-            indices = find_indices(mark[2, 0, :], marker_anato[2, 0, :])
-
-            init_anato = find_initial_height(marker_anato[2, 0, :])
+            indices = find_indices(mark[2, 1, :], marker_anato[2, 1, :])
+            md = find_min(mark[2, 1, :], indices)
             events.append(indices)
-
-            # plt.figure()
-            # plt.plot(mark[2, 0, :])
-            # plt.plot(marker_anato[2, 0, :], 'r')
-            # plt.plot([0, 1900], [init_anato, init_anato], 'k--')
-            # # plt.plot([0, 1900], [init_anato - 5, init_anato - 5], 'g--')
-            # for idx in indices:
-            #     plt.plot([idx, idx], [700, 1050], 'g--')
-            # plt.show()
-        return events
+            mid_events.append(md)
+        return events, mid_events
 
     def get_time(self):
         time = []
