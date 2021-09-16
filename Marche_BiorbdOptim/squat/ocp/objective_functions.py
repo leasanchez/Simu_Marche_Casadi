541d4e01e7b8a67b9d711aa9ebf6610afc078ac0
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-09-16 16:36:41 -0400

    Add objective fcn for exp data

--- Marche_BiorbdOptim/squat/ocp/objective_functions.py
+++ Marche_BiorbdOptim/squat/ocp/objective_functions.py
@@ -1,8 +1,9 @@
-from bioptim import ObjectiveFcn, Node, PenaltyNode, Axis
+from bioptim import ObjectiveFcn, Node, PenaltyNodeList, Axis, BiorbdInterface
 from casadi import MX, vertcat
+import numpy as np
 
 
-def sym_forces(pn: PenaltyNode) -> MX:
+def sym_forces(pn: PenaltyNodeList) -> MX:
     ns = pn.nlp.ns # number of shooting points
     nc = pn.nlp.model.nbContacts() # number of contact forces
     val = []  # init
@@ -14,12 +15,26 @@ def sym_forces(pn: PenaltyNode) -> MX:
             val = vertcat(val, (force[c]**2 - force[c+int(nc/2)]**2))
     return val
 
-def custom_CoM_position(pn: PenaltyNode) -> MX:
-    compute_CoM = pn.nlp.add_casadi_func("CoM", pn.nlp.model.CoM, pn.nlp.states["q"].mx)
-    com = compute_CoM(pn["q"])
-    return com[2]
-
 class objective:
+    @staticmethod
+    def set_objectif_function_exp(objective_functions, q_ref, mark_ref):
+        # 2D - objective fcn
+        # objective_functions.add(ObjectiveFcn.Lagrange.TRACK_STATE, key="q", target=q_ref, node=Node.ALL, weight=1)
+        # objective_functions.add(ObjectiveFcn.Lagrange.TRACK_STATE, key="qdot", weight=0.001)
+        # objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=0.001)
+
+        # 3D - objective fcn
+        marker_index = [26, 31, 46, 51] # foot markers
+        # objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=0.001, expand=False)
+        # objective_functions.add(ObjectiveFcn.Lagrange.TRACK_STATE, key="q", index=range(3), target=q_ref[:3, :], node=Node.ALL, weight=1)
+        objective_functions.add(ObjectiveFcn.Lagrange.TRACK_MARKERS,
+                                marker_index=marker_index,
+                                target=mark_ref[:, marker_index, :-1],
+                                node=Node.ALL,
+                                weight=10,
+                                expand=True)
+        objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_COM_POSITION, axes=Axis.Z, node=Node.MID, expand=True)
+
     @staticmethod
     def set_objectif_function(objective_functions, position_high, position_low, muscles=True):
         # --- control minimize --- #
