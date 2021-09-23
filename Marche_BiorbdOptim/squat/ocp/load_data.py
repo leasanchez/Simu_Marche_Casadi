e32995194593fc62621cc72a7c79566943d7dd56
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-09-23 10:17:31 -0400

    Load markers trajectories

--- Marche_BiorbdOptim/squat/ocp/load_data.py
+++ Marche_BiorbdOptim/squat/ocp/load_data.py
@@ -10,3 +10,8 @@ class data:
     def get_muscle_activation(name, title):
         activation = np.load("/home/leasanchez/programmation/Simu_Marche_Casadi/Marche_BiorbdOptim/squat/Data_test/" + name + "/EMG/" + title + "_mean.npy")
         return activation
+
+    @staticmethod
+    def get_markers_position(name, title):
+        markers_position = np.load("/home/leasanchez/programmation/Simu_Marche_Casadi/Marche_BiorbdOptim/squat/Data_test/" + name + "/MARKERS/" + title + "_mean.npy")
+        return markers_position
