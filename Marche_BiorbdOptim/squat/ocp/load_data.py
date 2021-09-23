630fcc6aa363764c6f9ac3ca39c76167bb49b6e9
Lea Sanchez <lea.sanchez@umontreal.ca>
2021-09-23 10:15:49 -0400

    Load muscle activation

--- Marche_BiorbdOptim/squat/ocp/load_data.py
+++ Marche_BiorbdOptim/squat/ocp/load_data.py
@@ -5,3 +5,8 @@ class data:
     def get_q(name, title):
         q = np.load("/home/leasanchez/programmation/Simu_Marche_Casadi/Marche_BiorbdOptim/squat/Data_test/" + name + "/kalman/" + title + "_mean.npy")
         return q
+
+    @staticmethod
+    def get_muscle_activation(name, title):
+        activation = np.load("/home/leasanchez/programmation/Simu_Marche_Casadi/Marche_BiorbdOptim/squat/Data_test/" + name + "/EMG/" + title + "_mean.npy")
+        return activation
